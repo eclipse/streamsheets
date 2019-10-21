756ce37be1def7a3ee8290c5c70951625758dbde
Philip Ackermann <info@philipackermann.de>
2019-10-21 15:43:16 +0200

    Add migration script to Raspberry Pi version

--- deployment/rpi/installer/scripts/start.sh
+++ deployment/rpi/installer/scripts/start.sh
@@ -3,4 +3,11 @@ SCRIPTPATH="$(cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 sudo docker network ls | grep streamsheets > /dev/null || sudo docker network create streamsheets
 sudo rm /internal-mongo/data/db/mongod.lock
+
+SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+"${SCRIPT_LOCATION}/migrate.sh" --quiet
+
+docker volume ls | grep streamsheets-data > /dev/null || docker volume create streamsheets-data
+
 sudo docker-compose up
