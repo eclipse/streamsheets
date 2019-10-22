1a45c45fdaaf0c0b660b832d18a8d5cc18caefd2
Andreas Sayn <andreas.sayn@cedalo.com>
2019-10-22 14:52:04 +0200

    Function Module: initial commit

---
+++ packages/functions/src/function-type.ts
@@ -0,0 +1,97 @@
+export const PARAM_TYPES = {
+	NUMBER: 'NUMBER',
+	INT: 'INT',
+	TEXT: 'TEXT',
+	BOOL: 'BOOL',
+	ENUM: 'ENUM',
+	JSON: 'JSON',
+	REFERENCE: 'REFERENCE',
+	LIST: 'LIST',
+	ONEOF: 'ONEOF',
+	ANY: 'ANY'
+} as const;
+
+type OneOf<T extends Array<CombinedSheetTypes>> = {
+	name: 'ONEOF';
+	types: T;
+};
+
+type List<T extends CombinedSheetTypes> = {
+	name: 'LIST';
+	type: T;
+};
+
+type Any = {
+	name: 'ANY';
+};
+
+type Number = {
+	name: 'NUMBER';
+	min?: number;
+	max?: number;
+};
+
+type Int = {
+	name: 'INT';
+	min?: number;
+	max?: number;
+};
+
+type Text = {
+	name: 'TEXT';
+};
+
+type Json = {
+	name: 'JSON';
+};
+
+type Bool = {
+	name: 'BOOL';
+	strict?: boolean;
+};
+
+type Reference = {
+	name: 'REFERENCE';
+};
+
+type NonRecursiveTypes = Any | Number | Int | Bool | Json | Text | Reference;
+
+type SheetTypes = NonRecursiveTypes | List<SheetTypes2> | Json | OneOf<Array<Exclude<SheetTypes2, OneOf<any[]>>>>;
+type SheetTypes2 = NonRecursiveTypes | List<SheetTypes3> | Json | OneOf<Array<Exclude<SheetTypes3, OneOf<any[]>>>>;
+type SheetTypes3 = NonRecursiveTypes | List<SheetTypes4> | Json | OneOf<Array<Exclude<SheetTypes4, OneOf<any[]>>>>;
+type SheetTypes4 = NonRecursiveTypes | List<SheetTypes5> | Json | OneOf<Array<Exclude<SheetTypes5, OneOf<any[]>>>>;
+type SheetTypes5 = NonRecursiveTypes | List<Any> | Json;
+
+type CombinedSheetTypes = SheetTypes | SheetTypes2 | SheetTypes3 | SheetTypes4 | SheetTypes5;
+
+export interface I18N {
+	de: string;
+	en: string;
+}
+
+interface BaseSheetParam {
+	id: string;
+	name: I18N;
+	type: SheetTypes;
+	description: I18N;
+}
+
+export interface RequiredSheetParam extends BaseSheetParam {}
+
+export interface OptionalSheetParam extends BaseSheetParam {
+	optional: true;
+}
+
+export interface DefaultingSheetParam extends BaseSheetParam {
+	defaultValue: any;
+}
+
+export type SheetParam = RequiredSheetParam | OptionalSheetParam | DefaultingSheetParam;
+
+export interface SheetFunction {
+	name: I18N;
+	description: I18N;
+	parameters: Array<SheetParam>;
+	repeatParams?: Array<string>;
+}
+
---
+++ packages/functions/src/functions/lookup.ts
@@ -0,0 +1,342 @@
+import { PARAM_TYPES, SheetFunction } from '../function-type';
+
+export const CHOOSE: SheetFunction = {
+	name: {
+		de: 'CHOOSE',
+		en: 'CHOOSE'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'number',
+			name: {
+				de: 'Nummer',
+				en: 'number'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'value',
+			name: {
+				de: 'value',
+				en: 'value'
+			},
+			type: { name: PARAM_TYPES.ANY },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: 'ansi'
+		}
+	],
+	repeatParams: ['value']
+};
+
+export const COLUMN: SheetFunction = {
+	name: {
+		de: 'COLUMN',
+		en: 'COLUMN'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'reference',
+			name: {
+				de: 'reference',
+				en: 'reference'
+			},
+			type: { name: PARAM_TYPES.REFERENCE },
+			description: {
+				de: '',
+				en: ''
+			},
+			optional: true
+		}
+	]
+};
+
+export const INDEX: SheetFunction = {
+	name: {
+		de: 'INDEX',
+		en: 'INDEX'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'range',
+			name: {
+				de: 'range',
+				en: 'range'
+			},
+			type: { name: PARAM_TYPES.REFERENCE },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'rowOffset',
+			name: {
+				de: 'rowOffset',
+				en: 'rowOffset'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'colOffset',
+			name: {
+				de: 'colOffset',
+				en: 'colOffset'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'areanr',
+			name: {
+				de: 'areanr',
+				en: 'areanr'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: 0
+		}
+	]
+};
+
+export const MATCH: SheetFunction = {
+	name: {
+		de: 'MATCH',
+		en: 'MATCH'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'value',
+			name: {
+				de: 'value',
+				en: 'value'
+			},
+			type: { name: PARAM_TYPES.ANY },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'range',
+			name: {
+				de: 'range',
+				en: 'range'
+			},
+			type: { name: PARAM_TYPES.REFERENCE },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'type',
+			name: {
+				de: 'type',
+				en: 'type'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: 1
+		}
+	]
+};
+
+export const OFFSET: SheetFunction = {
+	name: {
+		de: 'OFFSET',
+		en: 'OFFSET'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'range',
+			name: {
+				de: 'range',
+				en: 'range'
+			},
+			type: { name: PARAM_TYPES.REFERENCE },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'row',
+			name: {
+				de: 'row',
+				en: 'row'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'col',
+			name: {
+				de: 'col',
+				en: 'col'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: 1
+		},
+		{
+			id: 'height',
+			name: {
+				de: 'height',
+				en: 'height'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: -1
+		},
+		{
+			id: 'width',
+			name: {
+				de: 'width',
+				en: 'width'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: -1
+		}
+	]
+};
+
+export const ROW: SheetFunction = {
+	name: {
+		de: 'ROW',
+		en: 'ROW'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'reference',
+			name: {
+				de: 'reference',
+				en: 'reference'
+			},
+			type: { name: PARAM_TYPES.REFERENCE },
+			description: {	
+				de: '',
+				en: ''
+			},
+			optional: true
+		}
+	]
+};
+
+export const VLOOKUP: SheetFunction = {
+	name: {
+		de: 'VLOOKUP',
+		en: 'VLOOKUP'
+	},
+	description: {
+		de: '',
+		en: ''
+	},
+	parameters: [
+		{
+			id: 'value',
+			name: {
+				de: 'value',
+				en: 'value'
+			},
+			type: { name: PARAM_TYPES.ANY },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'range',
+			name: {
+				de: 'range',
+				en: 'range'
+			},
+			type: { name: PARAM_TYPES.REFERENCE },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'index',
+			name: {
+				de: 'index',
+				en: 'index'
+			},
+			type: { name: PARAM_TYPES.NUMBER },
+			description: {
+				de: '',
+				en: ''
+			}
+		},
+		{
+			id: 'exact',
+			name: {
+				de: 'exact',
+				en: 'exact'
+			},
+			type: { name: PARAM_TYPES.BOOL },
+			description: {
+				de: '',
+				en: ''
+			},
+			defaultValue: true
+		}
+	]
+};
