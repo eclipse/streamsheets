81bf491b10e25b48e102765e6839867d6c121327
tenhouben <arnd.houben@cedalo.com>
2019-10-30 18:43:44 +0100

    commons: initial, contains utility methods used in several different modules

---
+++ packages/commons/test/jest.config.js
@@ -0,0 +1,10 @@
+/**
+ * NOTE: 
+ *  - used to run a single test-file with cli option: --config
+ *  - to run ALL tests use $>npm t
+ */
+module.exports = {
+	bail: true,
+	collectCoverage: false,
+	verbose: true
+};
