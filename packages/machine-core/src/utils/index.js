6097a67c9f51917c798336ea8cd4dc50ad82bcea
tenhouben <arnd.houben@cedalo.com>
2019-11-07 14:21:55 +0100

    machine-core: removed comment out code from utils/index.js

--- packages/machine-core/src/utils/index.js
+++ packages/machine-core/src/utils/index.js
@@ -2,12 +2,10 @@ const update = require('./update');
 const properties = require('./properties');
 const isEmpty = require('./isEmpty');
 const isType = require('./isType');
-// const functions = require('./functions'); circular dependency
 
 
 module.exports = {
 	...isEmpty,
-	// ...functions,
 	...properties,
 	...update,
 	isType
