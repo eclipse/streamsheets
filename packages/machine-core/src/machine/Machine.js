67da7cf9fdc18f3adff9896856a1cd36a2c00381
tenhouben <arnd.houben@cedalo.com>
2019-11-07 10:51:32 +0100

    machine-core: Machine uses function utilities instead of FunctionRegistry

--- packages/machine-core/src/machine/Machine.js
+++ packages/machine-core/src/machine/Machine.js
@@ -1,4 +1,3 @@
-const IdGenerator = require('@cedalo/id-generator');
 const EventEmitter = require('events');
 const State = require('../State');
 const NamedCells = require('./NamedCells');
@@ -6,9 +5,10 @@ const Outbox = require('./Outbox');
 const StreamSheet = require('./StreamSheet');
 const locale = require('../locale');
 const logger = require('../logger').create({ name: 'Machine' });
-const FunctionRegistry = require('../FunctionRegistry');
 const Streams = require('../streams/Streams');
+const { getFunctionDefinitions, registerFunctionDefinitions } = require('../utils/functions');
 const { convert } = require('@cedalo/commons');
+const IdGenerator = require('@cedalo/id-generator');
 
 // REVIEW: move to streamsheet!
 const defaultStreamSheetName = (streamsheet) => {
@@ -97,12 +97,12 @@ class Machine {
 			settings: this.settings,
 			className: this.className,
 			namedCells: this.namedCells.getDescriptors(),
-			functionDefinitions: FunctionRegistry.functionDefinitions
+			functionDefinitions: getFunctionDefinitions()
 		};
 	}
 
 	load(definition = {}, functionDefinitions = [], currentStreams = []) {
-		FunctionRegistry.registerStreamFunctions(functionDefinitions);
+		registerFunctionDefinitions(functionDefinitions);
 		const def = Object.assign({}, DEF_CONF, definition);
 		const streamsheets = def.streamsheets || [];
 		this._id = def.isTemplate ? this._id : def.id || this._id;
@@ -147,13 +147,13 @@ class Machine {
 	}
 
 	loadFunctions(functionDefinitions = []) {
-		FunctionRegistry.registerStreamFunctions(functionDefinitions);
+		registerFunctionDefinitions(functionDefinitions);
 		this._streamsheets.forEach((streamsheet) => {
 			const { sheet } = streamsheet;
 			const json = sheet.toJSON();
 			sheet.load(json);
 		});
-		this._emitter.emit('update', 'functions', FunctionRegistry.functionDefinitions);
+		this._emitter.emit('update', 'functions', getFunctionDefinitions());
 	}
 
 	get id() {
