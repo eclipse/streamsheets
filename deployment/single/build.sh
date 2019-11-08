86445fee464ac1e87e82f666213bf347bd0b943e
Philip Ackermann <info@philipackermann.de>
2019-11-08 15:30:26 +0100

    Add tag to Docker image

--- deployment/single/build.sh
+++ deployment/single/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t cedalo/streamsheets .
+docker build -t cedalo/streamsheets:1.5-alpha .
--- deployment/single/start.sh
+++ deployment/single/start.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 docker run \
-  -p 27017:27017 \
-  -p 1883:1883 \
-  -p 6379:6379 \
-  -p 8082:8082 \
+  -p 8080:8080 \
+  -p 8081:8081 \
   -p 8083:8083 \
   -p 8088:8088 \
-  -p 8090:8090 \
-  -p 8091:8091 \
-  -p 9000:9000 \
   --env-file streamsheets.env \
-  cedalo/streamsheets
+  cedalo/streamsheets:1.5-alpha
