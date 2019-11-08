ebb879c50d3b220dc256b4a81fe79effa316ede1
tenhouben <arnd.houben@cedalo.com>
2019-11-08 18:41:04 +0100

    functions: fix test setup

--- packages/functions/test/setup.js
+++ packages/functions/test/setup.js
@@ -1,5 +1,5 @@
 const { functions } = require('../src/functions');
-const { SheetParser, SheetParserContext } = require('@cedalo/machine-core');
+const { SheetParser } = require('@cedalo/machine-core');
 
 // setup parser and its context...
-SheetParser.context = new SheetParserContext().registerFunctions(functions);
+Object.assign(SheetParser.context.functions, functions);
