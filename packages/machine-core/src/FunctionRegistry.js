64728a452f9924b5bf12136f660a22b068a9fab0
tenhouben <arnd.houben@cedalo.com>
2019-11-08 16:37:52 +0100

    machine-core: move functions handling to FunctionRegistry

--- packages/machine-core/src/FunctionRegistry.js
+++ packages/machine-core/src/FunctionRegistry.js
@@ -1,35 +1,73 @@
-const { SheetParser } = require('./parser/SheetParser');
-const logger = require('./logger').create({ name: 'function-utilities' });
+const logger = require('./logger').create({ name: 'FunctionRegistry' });
 
 // eslint-disable-next-line
 const requireModule = async (path) => require(path);
+
+let functionFactory;
+
+const Functions = {
+	core: {},
+	additional: {},
+	additionalHelp: {}
+};
+
+const registerCore = (functions = {}) => {
+	Functions.core = Object.assign(Functions.core, functions);
+};
+const registerAdditional = (functions = {}, help = {}) => {
+	Functions.additional = Object.assign(Functions.additional, functions);
+	Functions.additionalHelp = Object.assign(Functions.additionalHelp, help);
+};
+
+const toName = (name) => ({ name });
 
 class FunctionRegistry {
 	static of() {
 		return new FunctionRegistry();
 	}
 
+	getFunction(id = '') {
+		id = id.toUpperCase();
+		return Functions.core[id] || Functions.additional[id];
+	}
+
+	getFunctionDefinitions() {
+		// currently we only need the names...
+		return Object.keys(Functions.core).map(toName).concat(Object.keys(Functions.additional).map(toName));
+	}
+
+	getFunctionsHelp() {
+		return Functions.additionalHelp;
+	}
+
+	hasFunction(id = '') {
+		return !!this.getFunction(id);
+	}
+
 	registerCoreFunctionsModule(mod) {
 		requireModule(mod)
 			.then(({ functions, FunctionFactory }) => {
-				if (functions) SheetParser.context.registerFunctions(functions);
-				if (FunctionFactory) SheetParser.context.registerFunctionFactory(FunctionFactory);
+				registerCore(functions);
+				functionFactory = FunctionFactory;
 			})
 			.catch((err) => logger.info(err.message));
 	}
 
 	registerFunctionModule(mod) {
 		requireModule(mod)
-			.then(({ functions }) => functions && SheetParser.context.registerFunctions(functions))
+			.then(({ functions, help }) => registerAdditional(functions, help))
 			.catch((err) => logger.info(err.message));
 	}
 
-	registerFunctionDefinitions(fnDefinitions) {
-		SheetParser.context.registerFunctionDefinitions(fnDefinitions);
-	}
-
-	getFunctionDefinitions() {
-		return SheetParser.context.getFunctionDefinitions();
+	registerFunctionDefinitions(definitions = []) {
+		if (functionFactory) {
+			const functions = definitions.reduce((fns, def) => {
+				const fn = functionFactory.createFrom(def);
+				if (fn) fns[def.name] = fn;
+				return fns;
+			}, {});
+			registerAdditional(functions);
+		}
 	}
 }
 module.exports = FunctionRegistry.of();
