c95ad8f3db8d281e05b0c13b758b21c9e09be28d
tenhouben <arnd.houben@cedalo.com>
2019-11-21 16:45:37 +0100

    parser: start restructuring tests and add some additional parsing conditions checks

---
+++ packages/parser/test/condition.test.js
@@ -0,0 +1,120 @@
+const { Parser, ParserContext } = require('..');
+
+const DEF_CONTEXT = new ParserContext();
+
+const expectInfoHas = (info, has) => {
+	Object.entries(has).forEach(([key, value]) => {
+		try {
+			expect(info[key]).toBe(value);
+		} catch (err) {
+			throw new Error(
+				`Error: expect().toBe()\nKey: ${key}\nExpected value:\n\t${value}\nReceived:\n\t${info[key]}`
+			);
+		}
+	});
+};
+const expectConditionTerm = (term, condValue, leftValue, rightValue) => {
+	expect(term).toBeDefined();
+	expect(term.condition).toBeDefined();
+	expect(term.left).toBeDefined();
+	expect(term.right).toBeDefined();
+	expect(term.condition.value).toBe(condValue);
+	expect(term.left.value).toBe(leftValue);
+	expect(term.right.value).toBe(rightValue);
+};
+
+describe('parsing conditions', () => {
+	it('should parse conditions with ?', () => {
+		expect(Parser.parse('.5*?(3>2,250,"falsch")', DEF_CONTEXT).value).toBe(125);
+
+		expect(Parser.parse('?(4!=2,1,0)', DEF_CONTEXT).value).toBe(1);
+		expect(Parser.parse('?(4>=2,1,0)', DEF_CONTEXT).value).toBe(1);
+		expect(Parser.parse('?(4<=2,1,0)', DEF_CONTEXT).value).toBe(0);
+		expect(Parser.parse('?(-4<=2,1,0)', DEF_CONTEXT).value).toBe(1);
+		expect(Parser.parse('?(4==2,1,0)', DEF_CONTEXT).value).toBe(0);
+		expect(Parser.parse('?(4==4,1,0)', DEF_CONTEXT).value).toBe(1);
+
+		expect(Parser.parse('?(3+3>1 & 4+3<1, a, b)', DEF_CONTEXT).value).toBe('b');
+		expect(Parser.parse('?(3+3>1 | 4+3<1, a, b)', DEF_CONTEXT).value).toBe('a');
+
+		expect(Parser.parse('2+2*?(4>1,2,0)', DEF_CONTEXT).value).toBe(6);
+		expect(Parser.parse('.5*?(3>2,250,"falsch")', DEF_CONTEXT).value).toBe(125);
+		expect(Parser.parse('?(1+2>0,"ja", "no")', DEF_CONTEXT).value).toBe('ja');
+		expect(Parser.parse('?(5>1, "YES", "NO")', DEF_CONTEXT).value).toBe('YES');
+		expect(Parser.parse('?(3, ?(4 < 2, 42, 43), 44)', DEF_CONTEXT).value).toBe(43);
+		expect(Parser.parse('?(5>1, ?(5<1, "mm", 42), "NO")', DEF_CONTEXT).value).toBe(42);
+		expect(Parser.parse('?(3+2 > 1, ?(4 + 2 < 2, 42, 43), 44)', DEF_CONTEXT).value).toBe(43);
+
+		expect(Parser.parse('?(4,2,0)', DEF_CONTEXT).value).toBe(2);
+		expect(Parser.parse('?(4==,2,0)', DEF_CONTEXT).value).toBe(0);
+		expect(Parser.parse('?("hallo",2,0)', DEF_CONTEXT).value).toBe(2);
+	});
+	it('should parse conditions with IF', () => {
+		expect(Parser.parse('.5*IF(3>2,250,"falsch")', DEF_CONTEXT).value).toBe(125);
+		expect(Parser.parse('if ("IF",2,0)', DEF_CONTEXT).value).toBe(2);
+		expect(Parser.parse('iF("IF"=="if",2,4)', DEF_CONTEXT).value).toBe(4);
+		expect(Parser.parse('If (4<=2,1,0)', DEF_CONTEXT).value).toBe(0);
+		expect(Parser.parse('iF     (-4<=2,1,0)', DEF_CONTEXT).value).toBe(1);
+		// notes this:
+		expect(Parser.parse('iF + of', DEF_CONTEXT).value).toBe('iFof');
+	});
+	it('should support optional FALSE parameter', () => {
+		expect(Parser.parse('IF(4>2,2)', DEF_CONTEXT).value).toBe(2);
+		expect(Parser.parse('IF(4<2,2)', DEF_CONTEXT).value).toBe(null);
+		expect(Parser.parse('IF(4<2,3,"hi")', DEF_CONTEXT).value).toBe('hi');
+		expect(Parser.parse('IF(4>2,1)', DEF_CONTEXT).value).toBe(1);
+		expect(Parser.parse('IF(4<2,1)', DEF_CONTEXT).value).toBe(null);
+	});
+	it('should return string expression of parsed conditions', () => {
+		expect(Parser.parse('If (4<=2,1,0)', DEF_CONTEXT).toString()).toBe('IF(4<=2,1,0)');
+		expect(Parser.parse('if     (-4<=2,1,0)', DEF_CONTEXT).toString()).toBe('IF(-4<=2,1,0)');
+		expect(Parser.parse('?(5>1, "YES", "NO")', DEF_CONTEXT).toString()).toBe('?(5>1,"YES","NO")');
+		expect(Parser.parse('?(3+3>1 & 4+3<1, a, b))', DEF_CONTEXT).toString()).toBe('?(3+3>1&4+3<1,"a","b")');
+	});
+	it('should work with references', () => {
+		const testexpr = '?(Item.4!SYMBOL:reference!="","", "undefined")';
+		const term = Parser.parse(testexpr, DEF_CONTEXT);
+		expect(term.condition).toBeDefined();
+		expect(term.condition.left.value).toBe('Item.4!SYMBOL:reference');
+		expect(term.condition.operator.symbol).toBe('!=');
+	});
+});
+describe('parsing invalid conditions', () => {
+	const context = new ParserContext();
+	context.ignoreErrors = true;
+
+	it('should mark returned term as invalid', () => {
+		expect(Parser.parse('?(3+3 1 & 4+3<1, a)', context).isInvalid).toBeTruthy();
+		expect(Parser.parse('?(Item.4!SYMBOL:reference!="")', context).isInvalid).toBeTruthy();
+		expect(Parser.parse('?(3+2 > 1, ?(4 + 2 < 2, 42, 43, 44)', context).isInvalid).toBeTruthy();
+	});
+	it('should return recognized terms so far', () => {
+		const term = Parser.parse('if(1,"sa', context);
+		expect(term.isInvalid).toBe(true);
+		expect(term.condition).toBeDefined();
+		expect(term.condition.value).toBe(1);
+		expect(term.left).toBeDefined();
+		expect(term.left.value).toBe('sa');
+		expect(term.right).toBeDefined();
+		expect(term.right.value).toBe(null);
+		// additional tests
+		expectConditionTerm(Parser.parse('if(', context), null, null, null);
+		expectConditionTerm(Parser.parse('if(,,', context), null, null, null);
+		expectConditionTerm(Parser.parse('if(1', context), 1, null, null);
+		expectConditionTerm(Parser.parse('if(1,', context), 1, null, null);
+		expectConditionTerm(Parser.parse('if(1,,', context), 1, null, null);
+		expectConditionTerm(Parser.parse('if(1,,"h', context), 1, null, 'h');
+		expectConditionTerm(Parser.parse('if(1,,"h)', context), 1, null, 'h)');
+		expectConditionTerm(Parser.parse('if(1,(2', context), 1, 2, null);
+	});
+	test('parsing info of invalid condition', () => {
+		const info = Parser.getFormulaInfos('if(1,"sa', context);
+		expect(info).toBeDefined();
+		expect(info.length).toBe(4);
+		expectInfoHas(info[0], { start: 0, end: 7, paramIndex: undefined, type: 'condition' });
+		expectInfoHas(info[1], { start: 3, end: 4, paramIndex: 0, type: 'number', value: '1' });
+		expectInfoHas(info[2], { start: 5, end: 7, paramIndex: 1, type: 'string', value: 'sa' });
+		expectInfoHas(info[3], { start: 8, end: 8, paramIndex: 2, type: 'undef', value: undefined });
+	});
+});
+
