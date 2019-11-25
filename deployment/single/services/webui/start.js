6129fba3ae791bf6e8c4523080276e890734f814
tenhouben <arnd.houben@cedalo.com>
2019-11-25 15:20:00 +0100

    deployment: fix linting errors

--- deployment/single/services/webui/start.js
+++ deployment/single/services/webui/start.js
@@ -1,4 +1,5 @@
 const http = require('http');
+
 const port = 9000;
 
 const requestHandler = (request, response) => {
@@ -9,7 +10,8 @@ const server = http.createServer(requestHandler);
 
 server.listen(port, (error) => {
 	if (error) {
-		return console.error(error);
+		console.error(error);
+	} else {
+		console.log(`Web UI is listening on ${port}`);
 	}
-	console.log(`Web UI is listening on ${port}`);
 });
