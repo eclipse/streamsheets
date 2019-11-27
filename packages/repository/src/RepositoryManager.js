dc42c5ec5b89be2dc658e2e2052827e6549ea92c
Philip Ackermann <info@philipackermann.de>
2019-11-27 11:48:39 +0100

    Add method to popluate database based on initial configuration

--- packages/repository/src/RepositoryManager.js
+++ packages/repository/src/RepositoryManager.js
@@ -2,13 +2,46 @@ module.exports = class RepositoryManager {
 	static init({
 		graphRepository,
 		machineRepository,
+		streamRepositoryLegacy,
 		backupRestoreManager,
 		configurationRepository
 	}) {
 		RepositoryManager.graphRepository = graphRepository;
 		RepositoryManager.machineRepository = machineRepository;
+		RepositoryManager.streamRepositoryLegacy = streamRepositoryLegacy;
 		RepositoryManager.backupRestoreManager = backupRestoreManager;
 		RepositoryManager.configurationRepository = configurationRepository;
+	}
+
+	static async populateDatabases(initJSON) {
+		try {
+			const machines = initJSON.machines;
+			if (machines) {
+				for (const machineContainer of machines) {
+					try {
+						const { graph, machine} = machineContainer;
+						await RepositoryManager.graphRepository.saveGraph(graph);
+						await RepositoryManager.machineRepository.saveMachine(machine);
+					} catch (error) {
+						// ignore machine
+					}
+				}
+			}
+			const streams = initJSON.streams;
+			if (streams) {
+				for (const stream of streams) {
+					try {
+						// TODO: replace with stream repository procy
+						await RepositoryManager.streamRepositoryLegacy.saveConfiguration(stream);
+					} catch (error) {
+						// ignore stream
+						console.log(error)
+					}
+				}
+			}
+		} catch(error) {
+			// console.error(error);
+		}
 	}
 
 	static connectAll() {
