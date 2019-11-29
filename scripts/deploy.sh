292e2e3e006450734f7385f4d12c81e228547555
Philip Ackermann <info@philipackermann.de>
2019-11-29 12:25:06 +0100

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,8 @@
   --tag 1.5-alpha \
   --derivative dev \
   --service @cedalo/service-machines \
-  --service @cedalo/service-auth \
   --service @cedalo/service-streams \
   --service @cedalo/service-graphs \
-  --service @cedalo/service-opcua \
   --service @cedalo/gateway \
   --service @cedalo/webui
 #   --push
