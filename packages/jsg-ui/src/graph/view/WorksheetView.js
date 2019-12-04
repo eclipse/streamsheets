60ebc298532542e51de3f40468aeba0395cb2bda
TENRAUE <michael.raue@cedalo.com>
2019-12-04 15:25:55 +0100

    compensate rounding error when scrolling by key

--- packages/jsg-ui/src/graph/view/WorksheetView.js
+++ packages/jsg-ui/src/graph/view/WorksheetView.js
@@ -800,7 +800,7 @@ export default class WorksheetView extends ContentNodeView {
 			changed = true;
 		}
 
-		if (rect.getBottom() + offset.y > bounds.height - colSize) {
+		if (rect.getBottom() + offset.y - 1 > bounds.height - colSize) {
 			model = viewport.getVerticalRangeModel();
 			viewport.getVerticalRangeModel().setValue(model._min + rect.getBottom() - bounds.height);
 			changed = true;
