eb2ab29f9d30ca7945ca6eda566b1ad737ad9b37
tenhouben <arnd.houben@cedalo.com>
2019-12-05 19:09:07 +0100

    functions: add utility pipeCheckError() to exit pipe on function error

--- packages/functions/src/utils/common.js
+++ packages/functions/src/utils/common.js
@@ -1,3 +1,5 @@
+const { FunctionErrors } = require('@cedalo/error-codes');
+
 const pipe = (...fns) => (val) => fns.reduce((res, fn) => fn(res), val);
 const compose = (...fns) => (val) => fns.reduceRight((res, fn) => fn(res), val);
 const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
@@ -7,9 +9,24 @@ const cutBrackets = (str) => {
 	return str.endsWith(']') ? str.substr(0, str.length - 1) : str;
 };
 
+
+const ignoreOnError = (fn) => (res) => FunctionErrors.isError(res) ? res : fn(res);
+const wrapFunctions = (fn, fns) => fns.map(f => fn(f));
+
+const pipeCheckError = (...fns) => (val) => {
+	let res = val;
+	for (let i = 0; i < fns.length && !FunctionErrors.isError(res); i += 1) {
+		res = fns[i](res);
+	}
+	return res;
+};
+
 module.exports = {
 	compose,
 	cutBrackets,
 	deepCopy,
-	pipe
+	ignoreOnError,
+	pipe,
+	pipeCheckError,
+	wrapFunctions
 };
