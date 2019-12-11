a8f2ee2e1468e9d38efd96cdc575872a41d2f80c
Philip Ackermann <info@philipackermann.de>
2019-12-11 14:30:41 +0100

    Add colors to log messages of streamsheet script

--- deployment/single/streamsheets.sh
+++ deployment/single/streamsheets.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -e
+
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+
 FILE=/etc/mosquitto-default-credentials/pw.txt
 FILE_CLEAR=/etc/mosquitto-default-credentials/pw_clear.txt
 if [ -s $FILE ]
@@ -10,8 +18,9 @@ else
 	PASSWORD=$(openssl rand -base64 32)
 	mosquitto_passwd -b $FILE cedalo $PASSWORD
 	echo $PASSWORD >> $FILE_CLEAR
-	echo "Hashed password for Mosquitto broker is located in $FILE."
-	echo "Clear text password for Mosquitto broker is located in $FILE_CLEAR."
-	echo "For security reasons please copy the password from $FILE_CLEAR and delete that file afterwards."
+	echo -e "${GREEN}Password for default Mosquitto created successfully.${NOCOLOR}"
+	echo -e "Hashed password for Mosquitto broker is located in ${CYAN}$FILE${NOCOLOR}."
+	echo -e "Clear text password for Mosquitto broker is located in ${CYAN}$FILE_CLEAR${NOCOLOR}."
+	echo -e "${YELLOW}Warning: ${ORANGE}for security reasons please copy the password from $FILE_CLEAR and delete that file afterwards.${NOCOLOR}"
 fi
 /usr/bin/supervisord -c /etc/supervisord.conf
