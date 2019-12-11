d3d891e779f578957292c41ab5a5099c226a7cd0
Philip Ackermann <info@philipackermann.de>
2019-12-11 13:58:46 +0100

    Add script for generating a random password for Mosquitto

---
+++ deployment/single/streamsheets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+FILE=/etc/mosquitto-default-credentials/pw.txt
+FILE_CLEAR=/etc/mosquitto-default-credentials/pw_clear.txt
+if [ -s $FILE ]
+then
+	echo "Password for default Mosquitto exists"
+else
+	echo "Password for default Mosquitto does not exist, will create one."
+	touch $FILE
+	PASSWORD=$(openssl rand -base64 32)
+	mosquitto_passwd -b $FILE cedalo $PASSWORD
+	echo $PASSWORD >> $FILE_CLEAR
+	echo "Hashed password for Mosquitto broker is located in $FILE."
+	echo "Clear text password for Mosquitto broker is located in $FILE_CLEAR."
+	echo "For security reasons please copy the password from $FILE_CLEAR and delete that file afterwards."
+fi
