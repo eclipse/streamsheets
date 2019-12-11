12bdbf68285095e2872baf0206d0efb92729c341
Philip Ackermann <info@philipackermann.de>
2019-12-11 14:31:00 +0100

    Update start script for single Docker image deployment

--- deployment/single/start.sh
+++ deployment/single/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 docker run \
   -p 8081:8081 \
+  -v /tmp/streamsheets:/etc/mosquitto-default-credentials \
   -v /tmp/db:/var/lib/mongodb \
-  cedalo/streamsheets:1.5-alpha
+  cedalo/streamsheets:1.5-alpha.1
