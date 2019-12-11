5a8d789cfd68798d8bdbec696a256045ed3484c8
Philip Ackermann <info@philipackermann.de>
2019-12-11 15:16:05 +0100

    Update volumes in start script

--- deployment/single/start.sh
+++ deployment/single/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 docker run \
   -p 8081:8081 \
-  -v /tmp/streamsheets:/etc/mosquitto-default-credentials \
-  -v /tmp/db:/var/lib/mongodb \
+  -p 1883:1883 \
+  -v /tmp/streamsheets/mosquitto:/etc/mosquitto-default-credentials \
+  -v /tmp/streamsheets/db:/var/lib/mongodb \
   cedalo/streamsheets:1.5-alpha.1
