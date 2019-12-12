3cc4a240e4562e9d216b9439a8884857518afe60
Philip Ackermann <info@philipackermann.de>
2019-12-12 15:41:49 +0100

    Update test start script

--- deployment/single/start.sh
+++ deployment/single/start.sh
@@ -2,15 +2,20 @@
 
 docker network ls | grep streamsheets > /dev/null || docker network create streamsheets
 
-if [ ! "$(docker ps -a -q --no-trunc --filter name=^/streamsheets$)" ]
+docker volume ls | grep streamsheets-data > /dev/null || docker volume create streamsheets-data
+
+SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+STREAMSHEETS_CONTAINER_EXISTS="$(docker ps -a -q --no-trunc --filter name=^/streamsheets$)"
+
+if [ ! $STREAMSHEETS_CONTAINER_EXISTS ]
 then
 	echo "Creating and starting Streamsheets Docker container"
 	docker run \
 		-p 8081:8081 \
 		-p 8083:8083 \
 		-p 1883:1883 \
-		-v /tmp/streamsheets/mosquitto:/etc/mosquitto-default-credentials \
-		-v /tmp/streamsheets/db:/var/lib/mongodb \
+		-v $SCRIPT_LOCATION/settings/mosquitto:/etc/mosquitto-default-credentials \
+		-v streamsheets-data:/var/lib/mongodb \
 		--name streamsheets \
 		--network streamsheets \
 		cedalo/streamsheets:1.5-alpha.1
