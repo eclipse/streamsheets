f4b64433a68c5704799e2ce9dd8c95c47f930de9
Philip Ackermann <info@philipackermann.de>
2019-12-12 14:36:13 +0100

    Start existing streamsheets Docker container if exists otherwise create new

--- deployment/single/start.sh
+++ deployment/single/start.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
-docker run \
-  -p 8081:8081 \
-  -p 1883:1883 \
-  -v /tmp/streamsheets/mosquitto:/etc/mosquitto-default-credentials \
-  -v /tmp/streamsheets/db:/var/lib/mongodb \
-  cedalo/streamsheets:1.5-alpha.1
+if [ ! "$(docker ps -a -q --no-trunc --filter name=^/streamsheets$)" ]
+then
+	echo "Creating and starting Streamsheets Docker container"
+	docker run \
+		-p 8081:8081 \
+		-p 8083:8083 \
+		-p 1883:1883 \
+		-v /tmp/streamsheets/mosquitto:/etc/mosquitto-default-credentials \
+		-v /tmp/streamsheets/db:/var/lib/mongodb \
+		--name streamsheets \
+		cedalo/streamsheets:1.5-alpha.1
+else
+	echo "Starting Streamsheets Docker container"
+	docker start streamsheets -a
+fi
