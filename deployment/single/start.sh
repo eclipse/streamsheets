e1eb84adda22ad191e1bd9fe3a5e41073e3a7ad2
Philip Ackermann <info@philipackermann.de>
2019-12-12 14:36:28 +0100

    Create Docker network for streamsheets

--- deployment/single/start.sh
+++ deployment/single/start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+docker network ls | grep streamsheets > /dev/null || docker network create streamsheets
+
 if [ ! "$(docker ps -a -q --no-trunc --filter name=^/streamsheets$)" ]
 then
 	echo "Creating and starting Streamsheets Docker container"
@@ -9,6 +12,7 @@ then
 		-v /tmp/streamsheets/mosquitto:/etc/mosquitto-default-credentials \
 		-v /tmp/streamsheets/db:/var/lib/mongodb \
 		--name streamsheets \
+		--network streamsheets \
 		cedalo/streamsheets:1.5-alpha.1
 else
 	echo "Starting Streamsheets Docker container"
