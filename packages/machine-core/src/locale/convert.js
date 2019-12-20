cb1b3e007440fee1e961ad1261886d367df56027
tenhouben <arnd.houben@cedalo.com>
2019-12-20 18:22:00 +0100

    machine-core: fix converting number strings for different locales (DL-1737)

--- packages/machine-core/src/locale/convert.js
+++ packages/machine-core/src/locale/convert.js
@@ -1,27 +1,41 @@
-const replaceCharAt = (str, index, replacement) =>
-	(index >= 0 && index < str.length
-		? str.substr(0, index) + replacement + str.substr(index + 1)
-		: str);
+const memoize = (fn, keyfn) => {
+	const mem = {};
+	return (...args) => {
+		const key = keyfn(...args);
+		if (mem[key] == null) mem[key] = fn(...args);
+		return mem[key];
+	};
+};
 
-const isValidNrString = (str, thousandIndex, decimalIndex) =>
-	thousandIndex < 0 || (str.length - thousandIndex > 3 && (decimalIndex < 0 || decimalIndex - thousandIndex === 4));
+const replaceRegEx = memoize((str) => new RegExp(str, 'g'), (str) => str);
+const replaceAllWith = (replace, replacement, str) => {
+	if (replace === '.') replace = '\\.';
+	return str.replace(replaceRegEx(replace), replacement);
+};
 
+const nrChecker = memoize(
+	(separators) => {
+		const decimal = separators.decimal === '.' ? '\\.' : separators.decimal;
+		const thousand = separators.thousand === '.' ? '\\.' : separators.thousand;
+		return new RegExp(`^[-+]?(\\d{1,3})+(${thousand}\\d{3})*(${decimal}\\d*)?$`);
+	},
+	(separators) => `${separators.decimal}${separators.thousand}`
+);
+
+const isValidNr = (str, separators) => {
+	const rx = nrChecker(separators);
+	return rx.test(str);
+};
 const nrString = (str, locale) => {
 	let nr;
 	const separators = locale.separators;
-	const indexOfDeci = str.indexOf(separators.decimal);
-	const indexOfThou = str.indexOf(separators.thousand);
-	if (isValidNrString(str, indexOfThou, indexOfDeci)) {
-		nr = replaceCharAt(str, indexOfDeci, '.');
-		nr = replaceCharAt(nr, indexOfThou, '');
+	if (isValidNr(str, separators)) {
+		nr = replaceAllWith(separators.thousand, '', str);
+		nr = replaceAllWith(separators.decimal, '.', nr);
 	}
 	return nr;
 };
 
-const replaceAllWith = (replace, replacement, str) => {
-	if (replace === '.') replace = '\\.';
-	return str.replace(new RegExp(replace, 'g'), replacement);
-}
 const nrFormatString = (str, locale) => {
 	const separators = locale.separators;
 	let format = replaceAllWith(separators.decimal, ';', str);
