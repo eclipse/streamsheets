d5632a7cba5f810c98426e42f25a76bd78cd3b76
tenhouben <arnd.houben@cedalo.com>
2020-01-13 16:17:30 +0100

    functions: add help for new statistical functions

--- packages/functions/src/functions/statistical/help.js
+++ packages/functions/src/functions/statistical/help.js
@@ -3,10 +3,33 @@ module.exports = {
 	de: 'Statistik',
 	functions: {
 		AVERAGE: {
-			en: { argumentList: 'Value1,ValueN', description: 'Returns the average of specified cell values.' },
+			en: { argumentList: 'Value1,ValueN', description: 'Returns the average of specified cell values' },
 			de: {
 				argumentList: 'Wert1,WertN',
 				description: 'Bestimmt den Mittelwert über alle aufgelisteten Zellwerte'
+			}
+		},
+		AVERAGEIF: {
+			en: {
+				argumentList: 'CellRange,Criterion,AverageRange',
+				description:
+					'Returns the average of all values in provided CellRange or AverangeRange if given, which fulfill given criterion'
+			},
+			de: {
+				argumentList: 'Zellbereich,Kriterium,Durchschnittsbereich',
+				description:
+					'Bestimmt den Mittelwert über alle Werte im Zellbereich oder Durchschnittsbereich, falls angegeben, die das Kriterium erfüllen'
+			}
+		},
+		AVERAGEIFS: {
+			en: {
+				argumentList: 'AverageRange,Range1,Criterion1,RangeN,CriterionN',
+				description: 'Returns the average of all values in AverageRange which fulfill all given criteria'
+			},
+			de: {
+				argumentList: 'Durchschnittsbereich,Bereich1,Kriterium1,BereichN,KriteriumN',
+				description:
+					'Bestimmt den Mittelwert über alle Zellwerte aus dem Durchschnittsbereich die sämtliche Kriterien erfüllen'
 			}
 		},
 		CORREL: {
@@ -27,6 +50,26 @@ module.exports = {
 					'Zählt alle Werte, die einer Zahl entsprechen, aus den angegebenen Zellen oder Zellbereiche'
 			}
 		},
+		COUNTIF: {
+			en: {
+				argumentList: 'CellRange,Criterion',
+				description: 'Returns the number of cells in range which fulfills given criterion'
+			},
+			de: {
+				argumentList: 'Zellbereich,Kriterium',
+				description: 'Zählt alle Zellen im  Zellbereich, die das angegebene Kriterium erfüllen'
+			}
+		},
+		COUNTIFS: {
+			en: {
+				argumentList: 'CellRange1,Criterion1,CellRangeN,CriterionN',
+				description: 'Returns the number of cells which fulfill all specified criteria'
+			},
+			de: {
+				argumentList: 'Zellbereich1,Kriterium1,ZellbereichN,KriteriumN',
+				description: 'Zählt alle Zellen die sämtliche Kriterien erfüllen'
+			}
+		},
 		FORECAST: {
 			en: {
 				argumentList: 'Number_X,CellRange_Y,CellRange_X',
@@ -34,25 +77,45 @@ module.exports = {
 			},
 			de: {
 				argumentList: 'Zahl_X,Zellbereich_Y,Zellbereich_X',
-				description: 'Berechnet den Y-Wert zu gegebenen X-Wert, basierend auf bekannten Werten.'
+				description: 'Berechnet den Y-Wert zu gegebenen X-Wert, basierend auf bekannten Werten'
 			}
 		},
 		MAX: {
 			en: { argumentList: 'Value1,ValueN', description: 'Evaluates the maximum value' },
 			de: { argumentList: 'Wert1,WertN', description: 'Berechnet den größten Wert' }
 		},
+		MAXIFS: {
+			en: {
+				argumentList: 'MaxRange,CriterionRange1,Criterion1,CriterionRangeN,CriterionN',
+				description: 'Returns the maximum value of all cells which fulfill all specified criteria'
+			},
+			de: {
+				argumentList: 'MaxBereich,Kriterium1Bereich,Kriterium1,KriteriumNBereich,Kriterium1N',
+				description: 'Bestimmt den größten Wert aller Zellen die sämtliche Kriterien erfüllen'
+			}
+		},
 		MIN: {
 			en: { argumentList: 'Value1,Value2', description: 'Evaluates the minimum value' },
 			de: { argumentList: 'Wert1, Wert2', description: 'Berechnet den kleinsten Wert' }
 		},
-		'STDEV.S': {
+		MINIFS: {
 			en: {
-				argumentList: 'Number1,Number2,...',
-				description: 'Returns the standard derivation of given values.'
+				argumentList: 'MinRange,CriterionRange1,Criterion1,CriterionRangeN,CriterionN',
+				description: 'Returns the minimum value of all cells which fulfill all specified criteria'
 			},
 			de: {
-				argumentList: 'Zahl1,Zahl2,...',
-				description: 'Liefert die Standardabweichung zu den angegebenen Zahlen.'
+				argumentList: 'MinBereich,Kriterium1Bereich,Kriterium1,KriteriumNBereich,KriteriumN',
+				description: 'Bestimmt den kleinsten Wert aller Zellen die sämtliche Kriterien erfüllen'
+			}
+		},
+		'STDEV.S': {
+			en: {
+				argumentList: 'Number1,NumberN',
+				description: 'Returns the standard derivation of given values'
+			},
+			de: {
+				argumentList: 'Zahl1,ZahlN',
+				description: 'Liefert die Standardabweichung zu den angegebenen Zahlen'
 			}
 		}
 	}
