0af3eca1b89a1b6576387c76eaef5c47a45b5525
tenhouben <arnd.houben@cedalo.com>
2020-01-15 18:15:30 +0100

    service-graphs: handle load and streams-functions topics to extract function names for parsing (DL-3473)

--- packages/service-graphs/src/graph/SheetParserContext.js
+++ packages/service-graphs/src/graph/SheetParserContext.js
@@ -1,21 +1,27 @@
 const JSG = require('@cedalo/jsg-core');
 
-const SheetFunctions = require('./SheetFunctions');
-
 const { GraphParserContext } = JSG;
 
-const isFunctionParam = (node, parent) =>
-	parent && parent.type === 'function' && (node.type === 'string');
+const isFunctionParam = (node, parent) => parent && parent.type === 'function' && node.type === 'string';
 
+const FN_CALC = () => '#CALC';
+const ALL_FUNCTIONS = new Set();
 
 module.exports = class SheetParserContext extends GraphParserContext {
-
-	constructor() {
-		super();
-		this.functions = Object.assign(this.functions, SheetFunctions);
+	static updateFunctions(fnDefinitions = []) {
+		// seems we only need function names for copy paste => remove when c&p is done by machine server...
+		fnDefinitions.forEach((def) => ALL_FUNCTIONS.add(def.name));
 	}
 
 	createReferenceTerm(node, parent) {
-		return (isFunctionParam(node, parent)) ? undefined : super.createReferenceTerm(node);
+		return isFunctionParam(node, parent) ? undefined : super.createReferenceTerm(node);
+	}
+
+	hasFunction(id) {
+		return ALL_FUNCTIONS.has(id.toUpperCase());
+	}
+
+	getFunction(id) {
+		return this.hasFunction(id) ? FN_CALC : undefined;
 	}
 };
