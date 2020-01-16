288e8fb1f9c94eaf6b1416b623ca9a668de8df1f
TENRAUE <michael.raue@cedalo.com>
2020-01-16 16:52:37 +0100

    add professional version hint

--- packages/stubs/jsg-extensions/ui/SheetChartStateView.js
+++ packages/stubs/jsg-extensions/ui/SheetChartStateView.js
@@ -1,8 +1,15 @@
+import { default as JSG } from '@cedalo/jsg-core';
+
 export default function SheetChartStateViewFactory(JSG, ...args) {
 	class SheetChartStateView extends JSG.NodeView {
-		drawBorder(/* graphics, format, rect */) {}
-
-		drawFill(/* graphics, format, rect */) {}
+		drawFill(graphics, format, rect) {
+			const {x, y} = rect.getCenter();
+			super.drawFill(graphics, format, rect);
+			graphics.setFillColor('#444444');
+			graphics.setTextAlignment(JSG.TextFormatAttributes.TextAlignment.CENTER);
+			graphics.setTextBaseline('center');
+			graphics.fillText('This is a feature of the professional version!', x, y);
+		}
 	}
 	return new SheetChartStateView(...args);
 }
