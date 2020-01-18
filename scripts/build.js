609f28789dc70cf051aa9f14664f1257bace7c85
Andreas Sayn <andreas.sayn@cedalo.com>
2020-01-18 22:43:48 +0100

    deployment: update travis config to build single image version

---
+++ scripts/build.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console */
+const cp = require('child_process');
+const path = require('path');
+const argv = require('yargs').argv;
+
+const toArray = (something) => (Array.isArray(something) ? something : [something]);
+
+if (!argv.tag) {
+	console.error('Missing tag');
+	process.exit(1);
+}
+
+if (!argv.name) {
+	console.error('Missing name');
+	process.exit(1);
+}
+
+const tags = toArray(argv.tag);
+
+const push = argv.push;
+
+const baseImage = argv.image || '';
+
+const imageName = argv.name;
+
+const skipFrontendBuild = argv.skipFrontend;
+
+const baseImageArg = baseImage ? `--build-arg BASE_IMAGE=${baseImage}` : '';
+
+const buildTaggedImageArg = (tag) => `${imageName}:${tag}`;
+
+const dockerFilePath = path.join('deployment', 'single', 'Dockerfile');
+
+const taggedImageArgs = tags.map((tag) => `-t ${buildTaggedImageArg(tag)}`);
+
+try {
+	if (skipFrontendBuild) {
+		console.log('Skipping frontend build');
+	} else {
+		console.log(`Building frontend`);
+		cp.execSync('yarn build', {
+			stdio: 'inherit',
+			cwd: path.join('packages', 'webui')
+		});
+	}
+
+	console.log(`Building ${imageName}`);
+	cp.execSync(`docker build . -f ${dockerFilePath} ${baseImageArg} ${taggedImageArgs.join(' ')}`, {
+		stdio: 'inherit'
+	});
+
+	if (push) {
+		taggedImageArgs.forEach((image) => {
+			console.log(`Pushing image ${image}`);
+			cp.execSync(`docker push ${image}`, { stdio: 'inherit' });
+		});
+	}
+} catch (error) {
+	console.error(error);
+	process.exit(1);
+}
