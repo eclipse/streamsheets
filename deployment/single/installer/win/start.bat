bf94fb7d0ef993ce525605874a4c32a38a3a52d5
Philip Ackermann <info@philipackermann.de>
2020-01-23 11:46:24 +0100

    Prevent Windows start scripts from terminating terminal

--- deployment/single/installer/win/start.bat
+++ deployment/single/installer/win/start.bat
@@ -11,22 +11,21 @@ FOR /f "tokens=*" %%i in ('docker ps -a -q --no-trunc --filter name^=^^streamshe
 If "%1"=="pull" (
     echo "Checking for updates"
 	docker pull cedalo/streamsheets
-	EXIT 0
-)
-
-IF ["%STREAMSHEETS_CONTAINER_EXISTS%"] == [""] (
-	echo "Creating and starting Streamsheets Docker container"
-	md "%~dp0settings\mosquitto"
-	docker run ^
-		-p 8081:8081 ^
-		-p 8083:8083 ^
-		-p 1883:1883 ^
-		-v "%~dp0settings\mosquitto":/etc/mosquitto-default-credentials ^
-		-v streamsheets-data:/var/lib/mongodb ^
-		--name streamsheets ^
-		--network streamsheets ^
-		cedalo/streamsheets:1.5
 ) ELSE (
-	echo "Starting Streamsheets Docker container"
-	docker start streamsheets -a
+	IF ["%STREAMSHEETS_CONTAINER_EXISTS%"] == [""] (
+		echo "Creating and starting Streamsheets Docker container"
+		md "%~dp0settings\mosquitto"
+		docker run ^
+			-p 8081:8081 ^
+			-p 8083:8083 ^
+			-p 1883:1883 ^
+			-v "%~dp0settings\mosquitto":/etc/mosquitto-default-credentials ^
+			-v streamsheets-data:/var/lib/mongodb ^
+			--name streamsheets ^
+			--network streamsheets ^
+			cedalo/streamsheets:1.5
+	) ELSE (
+		echo "Starting Streamsheets Docker container"
+		docker start streamsheets -a
+	)
 )
