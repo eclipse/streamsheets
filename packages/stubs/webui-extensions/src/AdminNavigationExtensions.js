0b37e98dd31a67ff1b59fea77d82fea1a0e23be0
Philip Ackermann <info@philipackermann.de>
2020-02-10 16:13:29 +0100

    Create web UI extension for administrator navigation

---
+++ packages/stubs/webui-extensions/src/AdminNavigationExtensions.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-bitwise */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import IconPlugins from '@material-ui/icons/Build';
+
+import { MenuGroup, MenuEntry } from '@cedalo/webui-components';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export class AdminNavigationExtensions extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<MenuGroup
+				open={this.props.open}
+				onClick={() => this.props.setPluginsOpen(!this.props.open)}
+				label={<FormattedMessage id="Dashboard.plugins" defaultMessage="Plugins" />}
+				icon={<IconPlugins />}
+			>
+				<MenuEntry href="/administration/database" selected={this.props.isSelected('plugins')}>
+					<FormattedMessage id="Dashboard.database" defaultMessage="Database" />
+				</MenuEntry>
+			</MenuGroup>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		appState: state.appState,
+	};
+}
+
+export default connect(
+	mapStateToProps,
+)(AdminNavigationExtensions);
