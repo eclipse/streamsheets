eef18458d4d5ac47274d2701efbcae2f217c8b10
Philip Ackermann <info@philipackermann.de>
2020-02-11 14:56:45 +0100

    Add administration navigation extension to index

--- packages/stubs/webui-extensions/index.js
+++ packages/stubs/webui-extensions/index.js
@@ -1,5 +1,7 @@
+import AdminNavigationExtensions from './src/AdminNavigationExtensions';
 import ToolbarExtensions from './src/ToolbarExtensions';
 
 export {
+	AdminNavigationExtensions,
 	ToolbarExtensions
 }
