e8aae9b4240ab32ce568ffe3d801f1d8db711bc6
Philip Ackermann <info@philipackermann.de>
2020-02-11 15:39:59 +0100

    Add UI extension for plugins

---
+++ packages/stubs/webui-extensions/src/PluginExtensions.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-bitwise */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export class PluginExtensions extends Component {
+
+	render() {
+		return null;
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		appState: state.appState,
+	};
+}
+
+export default connect(
+	mapStateToProps,
+)(PluginExtensions);
