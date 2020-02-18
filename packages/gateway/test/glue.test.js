b8258c4a3511cfafd75fd27a0ba7c4f8860f0fa7
Andreas Sayn <andreas.sayn@cedalo.com>
2020-02-18 11:37:58 +0100

    gateway/tests: add method stub

--- packages/gateway/test/glue.test.js
+++ packages/gateway/test/glue.test.js
@@ -3,7 +3,7 @@ const glue = require('../src/glue.ts').default;
 
 describe('glue', () => {
 	test('returns api and auth', async () => {
-		const collection = { createIndexes: jest.fn() };
+		const collection = { createIndexes: jest.fn(), updateMany: jest.fn() };
 		const userRepo = await createUserRepository(collection);
 		const { api, auth } = glue({ userRepo });
 		expect(api).toBeDefined();
