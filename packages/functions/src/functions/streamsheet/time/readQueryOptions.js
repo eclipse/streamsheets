a8c2be54120dedbf6e64ba092826197a841eff97
tenhouben <arnd.houben@cedalo.com>
2020-02-26 09:36:09 +0100

    functions: rename helper mapQueryOptions to readQueryOptions

--- packages/functions/src/functions/streamsheet/time/readQueryOptions.js
+++ packages/functions/src/functions/streamsheet/time/readQueryOptions.js
@@ -8,50 +8,50 @@ const DEF_LIMIT = 100;
 const MIN_INTERVAL = 1 / 1000; // 1ms
 
 
-const State = {
+const Read = {
 	ERROR: (val, options) => {
 		options.error = val || ERROR.VALUE;
-		return State.ERROR;
+		return Read.ERROR;
 	},
 	QUERY: (val, options) => {
 		// is val query:
-		if (val != null && typeof val === 'object' && val.field != null) {
+		if (val != null && typeof val === 'object' && (val.value != null || val.values != null)) {
 			options.queries.push(val);
-			return State.QUERY;
+			return Read.QUERY;
 		}
-		return State.INTERVAL(val, options);
+		return Read.INTERVAL(val, options);
 	},
 	INTERVAL: (val, options) => {
 		const interval = val != null ? convert.toNumberStrict(val, 0) : -1;
 		if (interval < MIN_INTERVAL && interval !== -1) {
-			return State.ERROR(ERROR.VALUE, options);
+			return Read.ERROR(ERROR.VALUE, options);
 		}
 		options.interval = interval;
-		return State.RANGE;
+		return Read.RANGE;
 	},
 	RANGE: (val, options, term, sheet) => {
-		if (val == null) return State.LIMIT;
+		if (val == null) return Read.LIMIT;
 		options.range = getCellRangeFromTerm(term, sheet);
-		return options.range != null ? State.LIMIT : State.ERROR(ERROR.VALUE, options);
+		return options.range != null ? Read.LIMIT : Read.ERROR(ERROR.VALUE, options);
 	},
 	LIMIT: (val, options) => {
 		const limit = val != null ? convert.toNumberStrict(val) : DEF_LIMIT;
 		if (limit != null) {
 			options.limit = Math.max(1, limit);
-			return State.DONE;
+			return Read.DONE;
 		}
-		return State.ERROR(ERROR.VALUE, options);
+		return Read.ERROR(ERROR.VALUE, options);
 	},
-	DONE: () => State.DONE
+	DONE: () => Read.DONE
 };
 
-const mapQueryOptions = (sheet, terms) => {
-	let state = State.QUERY;
+const readQueryOptions = (sheet, terms) => {
+	let read = Read.QUERY;
 	const options = { queries: [], interval: -1, limit: DEF_LIMIT };
 	terms.forEach((term) => {
 		const val = term.value;
-		state = state(val, options, term, sheet);
+		read = read(val, options, term, sheet);
 	});
 	return options;
 };
-module.exports = mapQueryOptions;
+module.exports = readQueryOptions;
