35cd97615cc798353be77a24cdf5c469cdfe0f20
Philip Ackermann <info@philipackermann.de>
2020-03-05 13:38:48 +0100

    Add script for tagging image latest

---
+++ scripts/tag-latest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const cp = require('child_process');
+const path = require('path');
+const argv = require('yargs').argv;
+
+const fromImageTag = argv.fromImageTag || 'latest';
+
+const fromImage = `cedalo/streamsheets:${fromImageTag}`;
+const toImage = `cedalo/streamsheets:latest`;
+
+try {
+	console.log(`Pulling image ${fromImage}`);
+	cp.execSync(`docker pull ${fromImage}`, { stdio: 'inherit' });
+	console.log(`Tagging image ${toImage}`);
+	cp.execSync(`docker tag ${fromImage} ${toImage}`, { stdio: 'inherit' });
+	console.log(`Pushing image ${toImage}`);
+	cp.execSync(`docker push ${toImage}`, { stdio: 'inherit' });
+} catch (error) {
+	process.exit(1);
+}
