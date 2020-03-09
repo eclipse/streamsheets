55aef60fc4ada7737150712bbabb587f48d8cd6a
tenhouben <arnd.houben@cedalo.com>
2020-03-09 16:17:07 +0100

    functions: fix NOOP transform of timequery() to simply return passed entry

--- packages/functions/src/functions/streamsheet/time/transform.js
+++ packages/functions/src/functions/streamsheet/time/transform.js
@@ -2,7 +2,7 @@ const { common: { compose } } = require('../../../utils');
 const aggregations = require('./aggregations');
 const whereparser = require('./whereparser');
 
-const NOOP = () => 0;
+const NOOP = (entry) => entry;
 
 // const map = (mapFn) => (combineFn) => (a, c) => combineFn(a, mapFn(c));
 const reduce = (reduceFn) => (combineFn) => (a, c) => combineFn(reduceFn(a, c), c);
@@ -17,8 +17,6 @@ const createFrom = (query, period) => {
 	const aggregate = query.aggregate || [];
 	const whereCondition = whereparser.parseCondition(query.where);
 	if (whereCondition) xFormFns.push(filter(whereCondition));
-	// const methods = query.select.map((key, index) => reduce(aggregations.get(aggregate[index], key) || NOOP));
-	// const xform = compose(filter(interval(period)), filter(whereCondition), ...methods.values());
 	query.select.forEach((key, index) => xFormFns.push(reduce(aggregations.get(aggregate[index], key) || NOOP)));
 	const xform = compose(...xFormFns);
 	return xform(combine);
