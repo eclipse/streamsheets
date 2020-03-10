3697776ac6c7cd0ba1b65af99a8e1b6ba6cb2055
tenhouben <arnd.houben@cedalo.com>
2020-03-10 10:58:20 +0100

    functions: remove readQueryOptions in timequery.js because we go with a single query json only (WIP)

--- packages/functions/src/functions/streamsheet/time/timequery.js
+++ packages/functions/src/functions/streamsheet/time/timequery.js
@@ -1,15 +1,30 @@
+const { convert } = require('@cedalo/commons');
 const { FunctionErrors } = require('@cedalo/error-codes');
 const { Cell } = require('@cedalo/machine-core');
 const { Term } = require('@cedalo/parser');
 
-const { runFunction } = require('../../../utils');
-const readQueryOptions = require('./readQueryOptions');
+const { runFunction, terms: { getCellRangeFromTerm } } = require('../../../utils');
+// const readQueryOptions = require('./readQueryOptions');
 const stateListener = require('./stateListener');
 const transform = require('./transform');
 
 
 const ERROR = FunctionErrors.code;
+const DEF_LIMIT = 100;
+const MIN_INTERVAL = 1 / 1000; // 1ms
 
+
+const split = (str) => `${str}`.split(',').map((part) => part.trim());
+const createQuery = (json) => {
+	let query;
+	if (json != null && typeof json === 'object' && json.select) {
+		let aggregate = json.aggregate;
+		const { select, where } = json;
+		aggregate = aggregate != null ? split(aggregate) : [];
+		query = { select: split(select), aggregate, where };
+	}
+	return query;
+};
 
 const boundedPush = (size) => (value, entries) => {
 	entries.push(value);
@@ -17,13 +32,8 @@ const boundedPush = (size) => (value, entries) => {
 	return entries;
 };
 
-// const areEqualQueries = (query1, query2) => query1.value === query2.value && query1.aggregate ===  query2.aggregate;
-// const areEqualOptions = (opts1, opts2) =>
-// 	opts1.limit === opts2.limit &&
-// 	opts1.interval === opts2.interval &&
-// 	opts1.queries.length === opts2.queries.length &&
-// 	opts1.queries.every((query, index) => areEqualQueries(query, opts2.queries[index]));
-const areEqualOptions = (opts1, opts2) => opts1.limit === opts2.limit && opts1.interval === opts2.interval;
+const areEqualQueries = (q1, q2) => q1.select === q2.select && q1.aggregate === q2.aggregate && q1.where === q2.where;
+
 
 const entriesReduce = (all, { ts, values: vals }) => {
 	all.time.push(ts);
@@ -67,40 +77,49 @@ const spreadValuesToRange = (values, range) => {
 };
 
 class QueryStore {
-	static of(options) {
-		return new QueryStore(options);
+	static of(query, interval, limit) {
+		const store = new QueryStore(interval, limit);
+		store.queryobj = query;
+		store.xform = transform.createFrom(query, store.msInterval);
+		return store;
 	}
-	constructor({ interval, limit }) {
+	constructor(interval, limit) {
 		this.limit = limit;
-		this.push = boundedPush(limit);
-		this.interval = interval * 1000;
-		this.nextQuery = interval > 0 ? Date.now() + this.interval : -1;
+		this.interval = interval;
+		this.msInterval = interval * 1000;
+		this.nextQuery = interval > 0 ? Date.now() + this.msInterval : -1;
 		this.entries = [];
+		this.push = boundedPush(limit);
 		this.reset = this.reset.bind(this);
+		this.lastTry = 0;
 	}
 
 	reset() {
 		this.entries = [];
-		this.nextQuery = this.interval > 0 ? Date.now() + this.interval : -1;
+		this.nextQuery = this.msInterval > 0 ? Date.now() + this.msInterval : -1;
 	}
 
-	query(store, query, now = Date.now()) {
-		const xform = transform.createFrom(query, now - this.interval);
-		const result = store.entries.reduceRight(xform, { values: {} });
-		result.ts = now;
+	hasEqual(queryobj, interval, limit) {
+		return this.interval === interval && this.limit === limit && areEqualQueries(this.queryobj, queryobj);
+	}
+
+	query(store, now = Date.now()) {
+		// const xform = transform.createFrom(this.query, now - this.msInterval);
+		const result = store.entries.reduceRight(this.xform, { ts: now, values: {} });
 		// TODO: how to handle empty values in result, ie. result = { values: {} }??
 		this.push(result, this.entries);
 	}
-	queryOnInterval(store, query, now = Date.now()) {
+	queryOnInterval(store, now = Date.now()) {
 		if (this.nextQuery > 0 && now >= this.nextQuery) {
-			this.query(store, query, now);
-			this.nextQuery = now + this.interval;
+			this.query(store, now);
+			this.nextQuery = now + this.msInterval;
 		}
+		this.lastTry = now;
 	}
 
 	write(timestore, cell, range) {
 		let entries = this.entries;
-		if (this.interval < 0) {
+		if (this.msInterval < 0) {
 			entries = timestore.limit > this.limit ? limitEntries(timestore.entries, this.limit) : timestore.entries;
 		} 
 		const values = entries.reduce(entriesReduce, { time: [] });
@@ -109,15 +128,26 @@ class QueryStore {
 	}
 }
 
-const getQueryStore = (term, options) => {
-	if (!term._options || !term._querystore || !areEqualOptions(options, term._options)) {
-		term._querystore = QueryStore.of(options);
+const getQueryStore = (term, query, interval, limit) => {
+	if (!term._querystore || !term._querystore.hasEqual(query, interval, limit)) {
+		term._querystore = QueryStore.of(query, interval, limit);
 	}
 	// store current options which contains possible changed queries, required by tests...
-	term._options = options;
+	// term._options = options;
 	return term._querystore;
 };
 
+const getRange = (term, sheet) =>
+	term && term.value != null ? getCellRangeFromTerm(term, sheet) || ERROR.VALUE : undefined;
+const getNumber = (value, defVal) => value != null ? convert.toNumberStrict(value) : defVal;
+const getLimit = (nr) => {
+	nr = getNumber(nr, DEF_LIMIT);
+	return nr != null ? Math.max(1, nr) : ERROR.VALUE;
+}; 
+const getInterval = (nr) => {
+	nr = getNumber(nr, -1);
+	return nr != null && (nr >= MIN_INTERVAL || nr === -1) ? nr : ERROR.VALUE;
+};
 const storeFromTerm = (term) => term && term.name === 'time.store' ? term : undefined;
 const getTermFromRef = (cellref) => cellref.operand.target ? cellref.operand.target.term : undefined;
 const getStoreTerm = (term) => storeFromTerm(term) || storeFromTerm(getTermFromRef(term));
@@ -127,18 +157,19 @@ const timeQuery = (sheet, ...terms) =>
 	runFunction(sheet, terms)
 		.onSheetCalculation()
 		.withMinArgs(2)
+		.withMaxArgs(5)
 		.mapNextArg((storeref) => getStoreTerm(storeref) || ERROR.VALUE)
-		.mapRemaingingArgs((remain) => {
-			const options = readQueryOptions(sheet, remain);
-			return options.error || options;
-		})
-		.run((storeterm, options) => {
+		.mapNextArg((query) => createQuery(query.value) || ERROR.VALUE)
+		.mapNextArg((interval) => interval != null ? getInterval(interval.value) : -1)
+		.mapNextArg((range) => getRange(range))
+		.mapNextArg((limit) => limit != null ? getLimit(limit.value) : DEF_LIMIT)
+		.run((storeterm, query, interval, range, limit) => {
 			const term = timeQuery.term;
 			const timestore = storeterm._timestore;
-			const querystore = getQueryStore(term, options);
+			const querystore = getQueryStore(term, query, interval, limit);
 			stateListener.registerCallback(sheet, term, querystore.reset);
-			querystore.queryOnInterval(timestore, options.query);
-			querystore.write(timestore, term.cell, options.range);
+			querystore.queryOnInterval(timestore);
+			querystore.write(timestore, term.cell, range);
 			const size = querystore.entries.length;
 			// eslint-disable-next-line no-nested-ternary
 			return size === 0 ? ERROR.NA : size < querystore.limit ? true : ERROR.LIMIT;
