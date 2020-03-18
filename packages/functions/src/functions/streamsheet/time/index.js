2918b406ecb5a5f92eee986338a9efe709d62802
tenhouben <arnd.houben@cedalo.com>
2020-03-18 16:05:06 +0100

    functions: remove now obsolete time.interval function

--- packages/functions/src/functions/streamsheet/time/index.js
+++ packages/functions/src/functions/streamsheet/time/index.js
@@ -1,9 +1,7 @@
-const timeinterval = require('./timeinterval');
 const timequery = require('./timequery');
 const timestore = require('./timestore');
 
 module.exports = {
-	'TIME.INTERVAL': timeinterval,
-	'TIME.QUERY': timequery,
-	'TIME.STORE': timestore
+	'TIMEQUERY': timequery,
+	'TIMESTORE': timestore
 };
