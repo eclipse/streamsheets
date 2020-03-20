a20760a9327ec3f13492d2381c50fef797cfb1e5
Philip Ackermann <info@philipackermann.de>
2020-03-20 16:35:08 +0100

    Fix ESLint issue

--- packages/stubs/webui-extensions/index.js
+++ packages/stubs/webui-extensions/index.js
@@ -1,6 +1,7 @@
+import ImageUploadDialog from './src/components/ImageUploadDialog';
+
 export * from './src/AdminNavigationExtensions';
 export * from './src/PluginExtensions';
 export * from './src/ToolbarExtensions';
 export * from './src/languages';
-import ImageUploadDialog from './src/components/ImageUploadDialog';
 export { ImageUploadDialog } ;
