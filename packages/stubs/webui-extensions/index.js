af48f5ad18d4cca0f4133a3456e8d43211d694a3
Philip Ackermann <info@philipackermann.de>
2020-03-20 15:52:00 +0100

    Add stub for image upload dialog

--- packages/stubs/webui-extensions/index.js
+++ packages/stubs/webui-extensions/index.js
@@ -1,9 +1,11 @@
 import AdminNavigationExtensions from './src/AdminNavigationExtensions';
 import PluginExtensions from './src/PluginExtensions';
 import ToolbarExtensions from './src/ToolbarExtensions';
+import ImageUploadDialog from './src/components/ImageUploadDialog';
 
 export {
 	AdminNavigationExtensions,
 	PluginExtensions,
-	ToolbarExtensions
+	ToolbarExtensions,
+	ImageUploadDialog
 }
