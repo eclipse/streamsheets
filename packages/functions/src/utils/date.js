4ef7ee9d70a192a76e8d6b607fbde4ba5683faef
tenhouben <arnd.houben@cedalo.com>
2020-03-20 18:03:13 +0100

    functions: add localNow which respects local time offset to date utilities

--- packages/functions/src/utils/date.js
+++ packages/functions/src/utils/date.js
@@ -3,6 +3,9 @@ const HOUR_IN_SEC = 60 * MIN_IN_SEC;
 const DAY_IN_SEC = 24 * HOUR_IN_SEC;
 const DAY_IN_MS = 1000 * DAY_IN_SEC;
 
+const TZ_OFFSET_IN_MS = new Date().getTimezoneOffset() * 60000;
+
+
 // NOTE: new Date() automatically adds local timezone offset!!
 // const LOCAL_TIMEZONE_OFFSET = new Date().getTimezoneOffset() * MIN_IN_MS;
 
@@ -29,8 +32,10 @@ const serial2ms = serial => serial2date(serial).getTime();
 
 const time2serial = ms => ms2serial(ms) - 25569; // 25567 + 2;
 
+const localNow = () => Date.now() - TZ_OFFSET_IN_MS;
 
 module.exports = {
+	localNow,
 	ms2serial,
 	serial2date,
 	serial2ms,
