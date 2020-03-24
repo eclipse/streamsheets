bdcfedd41a74eb317b1e04dc4f8d29359db709f9
Andreas Sayn <andreas.sayn@cedalo.com>
2020-03-24 09:14:31 +0100

    Revert "webui-extensions: add loading of extension translations"
    This reverts commit 47016c139b70d7b90611afe49ff90249230623d4.

--- packages/stubs/webui-extensions/index.js
+++ packages/stubs/webui-extensions/index.js
@@ -3,7 +3,6 @@ import ImageUploadDialog from './src/components/ImageUploadDialog';
 export * from './src/AdminNavigationExtensions';
 export * from './src/PluginExtensions';
 export * from './src/ToolbarExtensions';
-export * from './src/languages';
 export * from './src/RoutesExtensions';
 export * from './src/WorkspaceSelect';
 export { ImageUploadDialog } ;
