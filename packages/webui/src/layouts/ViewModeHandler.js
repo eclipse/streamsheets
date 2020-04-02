b6ae1f868bd5c8cde8ec1f90cc2debd163fce7a7
tenhouben <arnd.houben@cedalo.com>
2020-04-02 19:32:13 +0200

    webui: preserve formerly set viewMode in ViewModeHandler

--- packages/webui/src/layouts/ViewModeHandler.js
+++ packages/webui/src/layouts/ViewModeHandler.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../actions/actions';
@@ -13,14 +14,15 @@ export const ViewModePropTypes = PropTypes.shape({
 function ViewModeHandlerComponent(props) {
 	const { searchParams } = props;
 	const params = new URLSearchParams(searchParams);
+	// set properties to undefined to keep currently set app state (e.g. in SharedMachinePage)
 	const viewMode = {
-		view: params.get('view'),
-		hidegrid: params.get('hidegrid'),
-		viewMode: params.get('viewmode'),
-		hideheader: params.get('hideheader'),
+		view: params.get('view') || undefined,
+		hidegrid: params.get('hidegrid') || undefined,
+		viewMode: params.get('viewmode') || undefined,
+		hideheader: params.get('hideheader') || undefined
 	};
-	props.setAppState({ viewMode });
-
+	// use effect to prevent endless update loop...
+	useEffect(() => props.setAppState({ viewMode }), []);
 	return null;
 }
 
