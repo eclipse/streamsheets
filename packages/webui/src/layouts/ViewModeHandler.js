a926335a14443abbdca16eabb6b386f6c2321713
tenhouben <arnd.houben@cedalo.com>
2020-04-05 15:38:56 +0200

    webui: ViewModeHandler sets view mode only if its passed by params to preserve current one in app-state

--- packages/webui/src/layouts/ViewModeHandler.js
+++ packages/webui/src/layouts/ViewModeHandler.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../actions/actions';
@@ -14,15 +13,16 @@ export const ViewModePropTypes = PropTypes.shape({
 function ViewModeHandlerComponent(props) {
 	const { searchParams } = props;
 	const params = new URLSearchParams(searchParams);
-	// set properties to undefined to keep currently set app state (e.g. in SharedMachinePage)
-	const viewMode = {
-		view: params.get('view') || undefined,
-		hidegrid: params.get('hidegrid') || undefined,
-		viewMode: params.get('viewmode') || undefined,
-		hideheader: params.get('hideheader') || undefined
-	};
-	// use effect to prevent endless update loop...
-	useEffect(() => props.setAppState({ viewMode }), []);
+	// if no params passed we keep those currently in app-state
+	if (searchParams) {
+		const viewMode = {
+			view: params.get('view'),
+			hidegrid: params.get('hidegrid'),
+			viewMode: params.get('viewmode'),
+			hideheader: params.get('hideheader')
+		};
+		props.setAppState({ viewMode });
+	}
 	return null;
 }
 
