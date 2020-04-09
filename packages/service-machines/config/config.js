1e3221398b787f7095d5325dcced452ac41a4313
tenhouben <arnd.houben@cedalo.com>
2020-04-09 16:28:49 +0200

    service-machines: support loading user defined functions

--- packages/service-machines/config/config.js
+++ packages/service-machines/config/config.js
@@ -2,7 +2,8 @@ module.exports = {
 	functions: {
 		// depth: 2,
 		ignore_dirs: ['node_modules'],
-		module_dir: process.env.FUNCTIONS_MODULE_DIR || 'functions'
+		module_dir: process.env.FUNCTIONS_MODULE_DIR || 'functions',
+		module_dir_user: process.env.USER_FUNCTIONS_MODULE_DIR || 'functions_user'
 	},
 	mongodb: {
 		MONGO_HOST: process.env.MONGO_HOST || 'localhost',
--- packages/service-machines/src/utils/FunctionModulesResolver.js
+++ packages/service-machines/src/utils/FunctionModulesResolver.js
@@ -1,23 +1,29 @@
+const logger = require('../utils/logger').create({ name: 'FunctionModuleResolver' });
 const path = require('path');
 const { resolve, resolveFile } = require('@cedalo/commons').moduleResolver;
 const config = require('../../config/config');
 
 const IGNORE_DIRS = config.functions.ignore_dirs;
-const FN_MODULES_DIR = config.functions.module_dir;
+const ADDITIONAL_FUNCTIONS = [config.functions.module_dir, config.functions.module_dir_user];
 const CORE_FUNCTIONS = '@cedalo/functions';
 
 const RESOLVED_MODULES = [];
 
-const traverse = async (dir) => {
+const traverse = async () => {
 	RESOLVED_MODULES.length = 0;
 	const corefn = resolveFile(CORE_FUNCTIONS);
 	if (corefn) RESOLVED_MODULES.push(corefn);
-	const modules = await resolve(path.resolve(dir), { ignoreDirs: IGNORE_DIRS });
-	RESOLVED_MODULES.push(...modules);
+	await Promise.all(
+		ADDITIONAL_FUNCTIONS.map((dir) =>
+			resolve(path.resolve(dir), { ignoreDirs: IGNORE_DIRS })
+				.then((modules) => RESOLVED_MODULES.push(...modules))
+				.catch((err) => logger.error(`Failed to resolve function module ${dir}`, err))
+		)
+	);
 	return RESOLVED_MODULES;
 };
 
 module.exports = {
-	resolve: async () => traverse(FN_MODULES_DIR),
+	resolve: traverse,
 	getModules: () => RESOLVED_MODULES
 };
