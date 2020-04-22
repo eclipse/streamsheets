c505cc1b53aa8a3ef6868c7a5bd42b9c610ff741
Philip Ackermann <info@philipackermann.de>
2020-04-22 14:12:49 +0200

    Clean up HTTP server configuration

--- packages/rest-server/src/RestServerProviderConfiguration.js
+++ packages/rest-server/src/RestServerProviderConfiguration.js
@@ -14,101 +14,10 @@ module.exports = class RestServerProviderConfiguration extends ProviderConfigura
 			label: 'REST URL'
 		});
 
-		this.addConnectorDefinition({
-			id: 'userName',
-			label: {
-				en: 'User Name',
-				de: 'Benutzername'
-			}
-		});
-
-		this.addConnectorDefinition({
-			id: 'password',
-			label: {
-				en: 'Password',
-				de: 'Kennwort'
-			},
-			type: ProviderConfiguration.FIELDTYPES.PASSWORD
-		});
-
-		this.addConsumerDefinition({
-			id: 'expectResponse',
-			label: {
-				en: 'Expect Response',
-				de: 'Antwort erwarten'
-			},
-			type: ProviderConfiguration.FIELDTYPES.CHECKBOX,
-			defaultValue: false
-		});
-
-		this.addConsumerDefinition({
-			id: 'responseTimeout',
-			label: {
-				en: 'Response Timeout',
-				de: 'Wartezeit auf Antwort'
-			},
-			dependsOnPath: 'expectResponse',
-			defaultValue: 5000
-		});
-
 		this.addConsumerDefinition({
 			id: 'topics',
 			label: 'URL Topics',
 			type: ProviderConfiguration.FIELDTYPES.TEXTLIST
 		});
-
-		this.addFunctionDefinition(this.respondFunction(requestId => ({
-			name: 'REST.RESPOND',
-			baseFunction: 'respond',
-			parameters: [
-				requestId,
-				{
-					id: 'body',
-					label: {
-						en: 'Body',
-						de: 'Body'
-					},
-					description: '',
-					type: {
-						name: 'union',
-						types: [
-							{ name: 'json' },
-							{ name: 'number' },
-							{ name: 'string' }
-						]
-					},
-					defaultValue: {}
-				},
-				{
-					id: 'statusCode',
-					label: {
-						en: 'Status Code',
-						de: 'Status Code'
-					},
-					description: '',
-					type: {
-						name: 'integer',
-						min: 100,
-						max: 599
-					},
-					defaultValue: 200
-				},
-				{
-					id: 'headers',
-					label: {
-						en: 'Headers',
-						de: 'Headers'
-					},
-					description: '',
-					type: {
-						name: 'json',
-						fieldType: {
-							name: 'string'
-						}
-					},
-					optional: true
-				}
-			]
-		})));
 	}
 };
