95e98c9a5f1c812f23cfc229ce7340c617ae720d
Andreas Sayn <andreas.sayn@cedalo.com>
2020-04-30 13:01:05 +0200

    Format

--- packages/stream/mqtt/src/MqttConsumer.js
+++ packages/stream/mqtt/src/MqttConsumer.js
@@ -23,20 +23,20 @@ module.exports = class MqttConsumer extends sdk.ConsumerMixin(MqttConnector) {
 	async initialize() {
 		let pass = true;
 		this.topics.forEach((t) => {
-			const {errors, warnings} = Utils.validateTopicForSubscribe(t);
-			errors.forEach(e => {
+			const { errors, warnings } = Utils.validateTopicForSubscribe(t);
+			errors.forEach((e) => {
 				this.handleError(new Error(e));
 			});
-			warnings.forEach(w => {
+			warnings.forEach((w) => {
 				this.handleWarning(new Error(w));
 			});
-			pass = pass && errors.length<1;
+			pass = pass && errors.length < 1;
 		});
-		if (pass){
+		if (pass) {
 			const options = {
-				properties : {}
+				properties: {}
 			};
-			if(this.hasUserProperties(this.config.userPropertiesSubscribe)) {
+			if (this.hasUserProperties(this.config.userPropertiesSubscribe)) {
 				options.properties.userProperties = this.config.userPropertiesSubscribe;
 			}
 			return new Promise((res, rej) => {
