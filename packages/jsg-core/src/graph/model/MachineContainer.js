925182da33ae6c56627050c2ea58c20ac9b28343
TENRAUE <michael.raue@cedalo.com>
2020-05-11 10:59:23 +0200

    fix outbox placement

--- packages/jsg-core/src/graph/model/MachineContainer.js
+++ packages/jsg-core/src/graph/model/MachineContainer.js
@@ -166,9 +166,9 @@ module.exports = class MachineContainer extends Node {
 			this._outboxContainer.setBoundingBoxTo(box);
 			this._outboxContainer.layout();
 		} else {
-			box.setLeft(0);
+			this._outboxContainer.getBoundingBox(box);
+			box.setLeft(box.getWidth() / 2);
 			box.setTop(0);
-			box.setWidth(0);
 			box.setHeight(0);
 			this._outboxContainer.setBoundingBoxTo(box);
 		}
