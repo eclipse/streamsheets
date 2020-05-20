fc4e72f96da488a136fb52b1db3d5eb91fe4ed3a
TENRAUE <michael.raue@cedalo.com>
2020-05-20 15:46:02 +0200

    reinsert removed function

--- packages/gateway-client/src/client/web/WebGatewayClient.js
+++ packages/gateway-client/src/client/web/WebGatewayClient.js
@@ -7,17 +7,17 @@ module.exports = class WebGatewayClient extends BaseGatewayClient {
 		super({ name, logger: console, defaultListener });
 	}
 
-	// _connectSocketServer(url) {
-	// 	return new Promise((resolve, reject) => {
-	// 		const ws = new WebSocket(url);
-	// 		ws.onopen = () =>
-	// 			this._handleOpenedSocketConnection().then(() => resolve(ws));
-	// 		ws.onmessage = (event) => this._handleSocketMessage(event.data);
-	// 		ws.onerror = (event) => {
-	// 			this._handleSocketError(event);
-	// 			reject(event);
-	// 		};
-	// 		ws.onclose = (event) => this._handleSocketClose(event);
-	// 	}).catch((error) => this._handleSocketError(error));
-	// }
+	_connectSocketServer(url) {
+		return new Promise((resolve, reject) => {
+			const ws = new WebSocket(url);
+			ws.onopen = () =>
+				this._handleOpenedSocketConnection().then(() => resolve(ws));
+			ws.onmessage = (event) => this._handleSocketMessage(event.data);
+			ws.onerror = (event) => {
+				this._handleSocketError(event);
+				reject(event);
+			};
+			ws.onclose = (event) => this._handleSocketClose(event);
+		}).catch((error) => this._handleSocketError(error));
+	}
 };
