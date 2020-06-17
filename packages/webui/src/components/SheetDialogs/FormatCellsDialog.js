007115d4f43218f19d6fc905c3d8e14df9dc607b
TENRAUE <michael.raue@cedalo.com>
2020-06-17 11:16:38 +0200

    translate none linestyle in format cells dialog

--- packages/webui/src/components/SheetDialogs/FormatCellsDialog.js
+++ packages/webui/src/components/SheetDialogs/FormatCellsDialog.js
@@ -1,7 +1,7 @@
 /********************************************************************************
  * Copyright (c) 2020 Cedalo AG
  *
- * This program and the accompanying materials are made available under the 
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
@@ -871,7 +871,10 @@ export class FormatCellsDialog extends React.Component {
 														value={name.value}
 														key={name.id}
 													>
-														{name.name}
+														<FormattedMessage
+															id="None"
+															defaultMessage="None"
+														/>
 													</MenuItem>
 												))}
 											</Select>
@@ -960,7 +963,10 @@ export class FormatCellsDialog extends React.Component {
 														value={name.value}
 														key={name.id}
 													>
-														{name.name}
+														<FormattedMessage
+															id="None"
+															defaultMessage="None"
+														/>
 													</MenuItem>
 												))}
 											</Select>
@@ -1049,7 +1055,10 @@ export class FormatCellsDialog extends React.Component {
 														value={name.value}
 														key={name.id}
 													>
-														{name.name}
+														<FormattedMessage
+															id="None"
+															defaultMessage="None"
+														/>
 													</MenuItem>
 												))}
 											</Select>
@@ -1137,7 +1146,10 @@ export class FormatCellsDialog extends React.Component {
 														value={name.value}
 														key={name.id}
 													>
-														{name.name}
+														<FormattedMessage
+															id="None"
+															defaultMessage="None"
+														/>
 													</MenuItem>
 												))}
 											</Select>
