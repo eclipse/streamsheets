05653ea9e829f818b97c618f93f36b22e16623cf
Philip Ackermann <info@philipackermann.de>
2020-06-30 16:21:41 +0200

    Add HTTP client adapter class

---
+++ packages/http-client/src/HTTPClient.js
@@ -0,0 +1,62 @@
+const logger = require('@cedalo/logger').create({ name: 'HTTP Client' });
+
+const axios = require('axios');
+
+const CONFIG = {};
+
+class HTTPClient {
+
+	constructor(config) {
+		this._init(config);
+	}
+
+	_init(config) {
+		this.id = IdGenerator.generate();
+	}
+
+	async request(config) {
+		returnaxios.request(config)
+
+	}
+
+	async get(url, config) {
+		return axios.get(url, config)
+	}
+
+	async delete(url, config) {
+		return axios.delete(url, config)
+	}
+
+	async head(url, config) {
+		return axios.head(url, config)
+	}
+
+	async options(url, config) {
+		return axios.options(url, config)
+	}
+
+	async post(url, data, config) {
+		return axios.post(url, data, config)
+	}
+
+	async put(url, data, config) {
+		return axios.put(url, data, config)
+	}
+
+	async patch(url, data, config) {
+		return axios.patch(url, data, config)
+	}
+
+}
+
+let instance;
+const getInstance = () => {
+	if (!instance) {
+		instance = new HTTPClient(CONFIG);
+	}
+	return instance;
+};
+
+module.exports = {
+	getInstance
+};
