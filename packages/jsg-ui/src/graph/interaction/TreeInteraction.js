c55365120474033c76ed647d92d842a5d5db552a
TENRAUE <michael.raue@cedalo.com>
2020-07-09 16:16:14 +0200

    draw loop icon next to key and allow scrolling in inbox, if machine is running

--- packages/jsg-ui/src/graph/interaction/TreeInteraction.js
+++ packages/jsg-ui/src/graph/interaction/TreeInteraction.js
@@ -1,7 +1,7 @@
 /********************************************************************************
  * Copyright (c) 2020 Cedalo AG
  *
- * This program and the accompanying materials are made available under the 
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
@@ -682,6 +682,7 @@ export default class TreeInteraction extends Interaction {
 			return;
 		}
 
+		view._didScroll = true;
 		const scrollView = view.getScrollView();
 		const pt = scrollView.getScrollPosition();
 
