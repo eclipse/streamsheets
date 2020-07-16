3596558e9ce5a4012bf3516ac4360cb8be768464
tenhouben <arnd.houben@cedalo.com>
2020-07-16 11:00:04 +0200

    functions: enhance stackupsert() to support operator strings created via formula (DL-4076)

--- packages/functions/src/functions/stack/stackupdater.js
+++ packages/functions/src/functions/stack/stackupdater.js
@@ -19,23 +19,26 @@ const operators = {
 };
 const set = (val) => () => val;
 const noop = () => (value) => value;
+const opfunc = (str, op) => {
+	const val = str.substring(1).trim();
+	return op(convert.toNumber(val, val));
+};
+const termfunc = (term) => term ? () => term.value : set(undefined);
+
 const operator = (str) => {
 	const op = operators[str.charAt(0)];
-	if (op) {
-		const val = str.substring(1).trim();
-		return op(convert.toNumber(val, val));
-	}
-	return set(str);
+	return op ? opfunc(str, op) : set(str);
 };
-
 const valueOperator = (cell) => {
 	const value = cell.value;
 	if (value != null) return isType.string(value) ? operator(value.trim()) : set(value);
 	return noop();
 };
 const formulaOperator = (cell) => {
-	const term = cell.term;
-	return term ? () => term.value : set(undefined);
+	// DL-4076: now requires to support that formula might returns an operator string
+	const cellvalue = cell.value;
+	const op = isType.string(cellvalue) && operators[cellvalue.charAt(0)];
+	return op ? opfunc(cellvalue, op) : termfunc(cell.term);
 };
 const cellOperator = (cell) => (cell.hasFormula ? formulaOperator(cell) : valueOperator(cell));
 
