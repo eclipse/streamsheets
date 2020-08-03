57a379b1459d2fd82f94e9d47b0a8e43e091e764
Philip Ackermann <info@philipackermann.de>
2020-08-03 13:00:02 +0200

    Add Streamsheets function PARSE.SVG()

--- packages/functions/src/functions/parse/parse.js
+++ packages/functions/src/functions/parse/parse.js
@@ -86,9 +86,24 @@ const markdown = (sheet, ...terms) =>
 				.reqId();
 		});
 markdown.displayName = true;
+
+const svg = (sheet, ...terms) =>
+	runFunction(sheet, terms)
+		.onSheetCalculation()
+		.withMinArgs(1)
+		.withMaxArgs(1)
+		.mapNextArg((string) => asString(string.value, ERROR.VALUE))
+		.run((string) => {
+			return AsyncRequest.create(sheet, svg.context)
+				.request(() => parseXML(string))
+				.response(createDefaultCallback())
+				.reqId();
+		});
+svg.displayName = true;
 module.exports = {
 	'PARSE.CSS': css,
 	'PARSE.CSV': csv,
 	'PARSE.JAVASCRIPT': javascript,
 	'PARSE.MARKDOWN': markdown,
+	'PARSE.SVG': svg,
 };
