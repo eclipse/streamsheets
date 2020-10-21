de4b19cf1b83cb86a32f0258fa39fafe6fd2a757
tenhouben <arnd.houben@cedalo.com>
2020-10-21 12:08:41 +0200

    functions: different implementation of sleep() to support usage outside (sheet-)cell (DL-4482)

--- packages/functions/src/functions/streamsheet/sleep.js
+++ packages/functions/src/functions/streamsheet/sleep.js
@@ -14,38 +14,65 @@ const {	runFunction } = require('../../utils');
 
 const ERROR = FunctionErrors.code;
 
-const pause = (sheet) => {
+
+const createPauseFn = (sheet) => () => {
 	if (!sheet.isPaused) sheet.pauseProcessing();
-	return true;
 };
-const resume = (sheet) => {
+const createResumeFn = (sheet) => () => {
 	if (sheet.isPaused) sheet.resumeProcessing();
-	return true;
 };
-
-const doResume = (context, seconds) => {
-	const now = Date.now();
-	const ms = seconds * 1000;
-	if (!context._lastSleep) context._lastSleep = now;
-	const resumeSleep = ms < 1 || now - context._lastSleep > ms;
-	if (resumeSleep) context._lastSleep = undefined;
-	return resumeSleep;
-};
-
-const initContext = (sheet, context) => {
-	if (!context._sleepInited) {
-		context._sleepInited = true;
-		context.addDisposeListener(() => resume(sheet));
+const clearResumeTimeout = (context) => {
+	if (context.resumeTimeoutId) {
+		clearTimeout(context.resumeTimeoutId);
+		context.resumeTimeoutId = undefined;
 	}
 };
-
+const resume = (context) => {
+	clearResumeTimeout(context);
+	context.resumeFn();	
+	context.resumed = true;
+};
+const pause = (context, ms) => {
+	clearResumeTimeout(context);
+	context.pauseFn();
+	context.period = ms;
+	context.resumed = false;
+	context.resumeTimeoutId = setTimeout(() => resume(context), ms);
+};
+const doSleep = (context, seconds) => {
+	const ms = seconds * 1000;
+	if (ms < 1) {
+		resume(context);
+		context.resumed = false;
+	} else if (context.hasCell) {
+		if (!context.resumeTimeoutId || ms !== context.period) {
+			if (!context.resumed) pause(context, ms);
+			else context.resumed = false;
+		}
+	} else {
+		// triggered outside of sheet, e.g. by button press. so create new timeout
+		pause(context, ms);
+	}
+	return true;
+};
+const initContext = (context, sheet) => {
+	if (!context.initialized) {
+		context.initialized = true;
+		context.pauseFn = createPauseFn(sheet);
+		context.resumeFn = createResumeFn(sheet);
+		// flags to handle usage in cell.
+		context.hasCell = !!context.term.cell;
+		context.resumed = false; // pause again in next step, not directly! 
+		context.addDisposeListener((ctxt) => resume(ctxt));
+	}
+};
 const sleep = (sheet, ...terms) =>
 	runFunction(sheet, terms)
 		.onSheetCalculation()
 		.withArgCount(1)
 		.mapNextArg((seconds) => convert.toNumberStrict(seconds.value, ERROR.VALUE))
-		.beforeRun(() => initContext(sheet, sleep.context))
-		.run((seconds) => doResume(sleep.context, seconds) ? resume(sheet) : pause(sheet));
+		.beforeRun(() => initContext(sleep.context, sheet))
+		.run((seconds) => doSleep(sleep.context, seconds));
 sleep.displayName = true;
 
 module.exports = sleep;
