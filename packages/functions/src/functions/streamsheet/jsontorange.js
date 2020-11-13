98356ae1b865bf5687f259525f1693acf4ab8418
tenhouben <arnd.houben@cedalo.com>
2020-11-13 17:37:09 +0100

    functions: review json.to.range() and transform json to 2d array so we can reuse logic for ranges

--- packages/functions/src/functions/streamsheet/jsontorange.js
+++ packages/functions/src/functions/streamsheet/jsontorange.js
@@ -17,43 +17,8 @@ const { runFunction, terms: { getCellRangeFromTerm, getJSONFromTerm } } = requir
 const ERROR = FunctionErrors.code;
 const TYPES = ['ARRAY', 'DICTIONARY', 'JSON', 'RANGE'];
 
+
 const termFromValue = (value) => (isType.object(value) ? new ObjectTerm(value) : Term.fromValue(value));
-const flattenArray = (arr, list=[]) => {
-	arr.forEach((value, index) => {
-		if (Array.isArray(value)) {
-			list.push({ key: index });
-			flattenArray(value, list);
-		} else if (isType.object(value)) {
-			list.push({ key: index });
-			// eslint-disable-next-line no-use-before-define
-			flattenObject(value, list);
-		} else {
-			list.push({ key: index, value });
-		}
-	});
-	return list;
-};
-const flattenObject = (obj, list = []) => {
-	const entries = Object.entries(obj);
-	entries.forEach(([key, value]) => {
-		if (Array.isArray(value)) {
-			list.push({ key });
-			flattenArray(value, list);
-		} else if (isType.object(value)) {
-			list.push({ key });
-			flattenObject(value, list);
-		} else {
-			list.push({ key, value });
-		}
-	});
-	return list;
-};
-const json2KeyValues = (json) => (Array.isArray(json) ? flattenArray(json) : flattenObject(json));
-const relativeIndex = (range, direction) => {
-	const startcol = range.start.col;
-	const startrow = range.start.row;
-	return (index) => direction ? index.row - startrow : index.col - startcol;
-};
 const setCellAt = (index, value, sheet) => {
 	// handle empty strings like undefined!
 	if (value == null || value === '') {
@@ -64,76 +29,24 @@ const setCellAt = (index, value, sheet) => {
 		cell.term = termFromValue(value);
 	}
 };
-const valueFromEntry = (entry, useKey) => {
-	if (entry == null) return entry;
-	const { key, value } = entry;
-	return useKey ? key : value;
-};
-const fillRangeByColumn = (range, keyValues, sheet) => {
-	let useKey = false;
-	const toArrayIndex = relativeIndex(range, true);
-	range.iterateByCol((cell, index, nextcol) => {
-		if (nextcol) useKey = !useKey;
-		const idx = toArrayIndex(index);
-		const entry = keyValues[idx];
-		const value = valueFromEntry(entry, useKey);
-		setCellAt(index, value, sheet);
-	});
-};
-const fillRangeByRow = (range, keyValues, sheet) => {
-	let useKey = false;
-	const toArrayIndex = relativeIndex(range, false);
-	range.iterate((cell, index, nextrow) => {
-		if (nextrow) useKey = !useKey;
-		const idx = toArrayIndex(index);
-		const entry = keyValues[idx];
-		const value = valueFromEntry(entry, useKey);
-		setCellAt(index, value, sheet);
-	});
-};
-const spreadJSON = (json, range, direction) => {
-	const sheet = range.sheet;
-	const keyValues = json2KeyValues(json);
-	if (direction) fillRangeByColumn(range, keyValues, sheet);
-	else fillRangeByRow(range, keyValues, sheet);
-};
 
-const autoSpreadJSON = (json, range, direction) => {
-	const sheet = range.sheet;
-	const index = range.start.copy();
-	const startcol = index.col;
-	const startrow = index.row;
-	const keyValues = json2KeyValues(json);
-	keyValues.forEach(({ key, value }, idx) => {
-		if (direction) {
-			index.set(startrow + idx, startcol);
-			setCellAt(index, key, sheet);
-			index.set(startrow + idx, startcol + 1);
-			setCellAt(index, value, sheet);
+const flattenObject = (obj, result) => {
+	const isObjArray = Array.isArray(obj);
+	Object.entries(obj).forEach(([key, value]) => {
+		// in case of array key is index
+		key = isObjArray ? Number(key) : key;
+		result[0].push(key);
+		if (Array.isArray(value) || isType.object(value)) {
+			result[1].push(undefined);
+			flattenObject(value, result);
 		} else {
-			// index.set(startrow + col, startcol + row);
-			index.set(startrow, startcol + idx);
-			setCellAt(index, key, sheet);
-			index.set(startrow + 1, startcol + idx);
-			setCellAt(index, value, sheet);
-		} 
+			result[1].push(value);
+		}
 	});
+	return result;
 };
-const spreadJSON2 = (json, range, direction) => {
-	let useKey = false;
-	const sheet = range.sheet;
-	const keyValues = json2KeyValues(json);
-	const toArrayIndex = relativeIndex(range, direction);
-	const iterateRange = (direction ? range.iterateByCol : range.iterate).bind(range);
-	iterateRange((cell, index, next) => {
-		if (next) useKey = !useKey;
-		const idx = toArrayIndex(index);
-		const entry = keyValues[idx];
-		const value = valueFromEntry(entry, useKey);
-		setCellAt(index, value, sheet);
-	});
-};
-
+// returns a 2D array with keys in first row and values in second => so we can handle it same as range!!
+const flattenJSON = (json) => flattenObject(json, [[], []]);
 
 const mapCol = (direction) => (direction ? (coord) => coord.x : (coord) => coord.y);
 const mapRow = (direction) => (direction ? (coord) => coord.y : (coord) => coord.x);
@@ -145,7 +58,7 @@ const mapValues = (values, direction) => {
 		return list ? list[col(coord)] : undefined;
 	};
 };
-const spreadLists = (lists, range, direction) => {
+const autoSpreadRange = (lists, range, direction) => {
 	const sheet = range.sheet;
 	const index = range.start.copy();
 	const startcol = index.col;
@@ -157,8 +70,10 @@ const spreadLists = (lists, range, direction) => {
 			setCellAt(index, value, sheet);
 		});
 	});
+	return true;
 };
 const spreadRange = (lists, range, direction) => {
+	if (range.width === 1 && range.height === 1) return autoSpreadRange(lists, range, direction);
 	const coord = { x: -1, y: -1 };
 	const sheet = range.sheet;
 	const getValue = mapValues(lists, direction);
@@ -171,16 +86,21 @@ const spreadRange = (lists, range, direction) => {
 		const value = getValue(coord);
 		setCellAt(index, value, sheet);
 	});
+	return true;
 };
-
+const ensureRange = (json) => {
+	if (Array.isArray(json)) {
+		const firstEntry = json[0];
+		return firstEntry != null && !Array.isArray(firstEntry) ? [json] : json;
+	}
+	return undefined;
+}
 const getType = (term) => {
 	let value = convert.toString(term.value);
 	if (value != null) value = value.toUpperCase();
 	// eslint-disable-next-line no-nested-ternary
 	return value == null ? 'JSON' : TYPES.includes(value) ? value : undefined;
 };
-// json should be an array of arrays
-const isRange = (json) => Array.isArray(json) && (json[0] == null || Array.isArray(json[0]));
 const getRange = (term) => {
 	const range = getCellRangeFromTerm(term);
 	return range == null || range.width > 0 || range.height > 0 ? range : undefined;
@@ -197,30 +117,18 @@ const jsontorange = (sheet, ...terms) =>
 			let res = true;
 			switch (type) {
 				case 'ARRAY':
-					if (Array.isArray(json)) {
-						// check if first entry is array too
-						const firstEntry = json[0];
-						if (firstEntry != null && !Array.isArray(firstEntry)) json = [json];
-						if (range.width === 1 && range.height === 1) spreadLists(json, range, direction);
-						else spreadRange(json, range, direction);
-					}
-					else res = ERROR.VALUE;
-					break;
 				case 'RANGE':
-					if (isRange(json)) {
-						if (range.width === 1 && range.height === 1) spreadLists(json, range, direction);
-						else spreadRange(json, range, direction);
-					}
-					else res = ERROR.VALUE;
+					json = ensureRange(json);
+					res = json ? spreadRange(json, range, direction) : ERROR.VALUE;
 					break;
 				case 'DICTIONARY':
 					//dictionary is simply JSON with flipped direction, so
 					direction = !direction;
-				// eslint-disable-next-line no-fallthrough
+					// eslint-disable-next-line no-fallthrough
 				default:
-					// JSON
-					if (range.width === 1 && range.height === 1) autoSpreadJSON(json, range, direction);
-					else spreadJSON(json, range, direction);
+					// flatten json to 2D array so we can reuse spreadRange()
+					json = flattenJSON(json);
+					spreadRange(json, range, !direction);
 			}
 			return res;
 		});
