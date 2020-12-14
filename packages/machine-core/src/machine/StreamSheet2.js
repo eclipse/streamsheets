efee66ce1aac517f3329a8973e465c1eed504174
tenhouben <arnd.houben@cedalo.com>
2020-12-14 14:33:52 +0100

    machine-core: remove commented out code in StreamSheet2

--- packages/machine-core/src/machine/StreamSheet2.js
+++ packages/machine-core/src/machine/StreamSheet2.js
@@ -58,16 +58,10 @@ class StreamSheet2 extends StreamSheet {
 			this.trigger.isActive = false;
 		}
 	}
-	// TODO maybe rename => called only by return function
 	stopProcessing(retval) {
 		this.trigger.stopProcessing();
 		this.sheet.stopProcessing(retval);
 		if (this.trigger.isEndless) this._msgHandler.next();
-		// this.trigger.stopRepeat();
-		// if (this.trigger.isEndless) {
-		// 	this._msgHandler.next();
-		// 	this.trigger.stopRepeat();
-		// }
 	}
 	pauseProcessing() {
 		this.sheet.pauseProcessing();
@@ -111,51 +105,10 @@ class StreamSheet2 extends StreamSheet {
 	postStep(manual) {
 		this.trigger.postStep(manual);
 	}
-	// _doStep(manual) {
-	// 	this._attachMessage2();
-	// 	const result = this.trigger.step(manual);
-	// 	if (this.sheet.isFinished) {
-	// 		if (!this.trigger.isEndless) this._msgHandler.next();
-	// 		this._detachMessage2();
-	// 	}
-	// 	return result;
-	// }
 
-	// called by trigger
-	// triggerStepORG() {
-		// 	// TODO: should we do this on step() ???
-		// 	this._attachMessage2();
-	// 	const result = this.sheet.startProcessing();
-	// 	if (this.sheet.isProcessed) {
-		// 		if (!this.trigger.isEndless) this._msgHandler.next();
-		// 		this._detachMessage2();
-		// 	}
-		// 	return result;
-		// }
-
-	// called by trigger
-	// repeatStep() {
-	// 	this.stats.repeatsteps += 1;
-	// 	this._doStep2();
-	// }
-	// // called by trigger
-	// cycleStep() {
-	// 	this.stats.steps += 1;
-	// 	this._doStep2();
-	// }
-	// _doStep2() {
-	// 	if (this.sheet.isReady || this.sheet.isProcessed) this._attachMessage3();
-	// 	const result = this.sheet.process();
-	// 	if (this.sheet.isProcessed) {
-	// 		// on endless we reuse message
-	// 		if (!this.trigger.isEndless) this._msgHandler.next();
-	// 		this._detachMessage2();
-	// 	}
-	// 	return result;
-	// }
 	triggerStep() {
-		if (this.sheet.isReady || this.sheet.isProcessed) this._attachMessage3();
-		const result = this.sheet.process();
+		if (this.sheet.isReady || this.sheet.isProcessed) this._attachMessage2();
+		const result = this.sheet.startProcessing();
 		if (this.sheet.isProcessed) {
 			// on endless we reuse message
 			if (!this.trigger.isEndless) this._msgHandler.next();
@@ -164,19 +117,8 @@ class StreamSheet2 extends StreamSheet {
 		this._emitter.emit('step', this);
 		return result;
 	}
-	// triggerStep(repeatstep) {
-	// 	if (repeatstep) this.stats.repeatsteps += 1;
-	// 	else this.stats.steps += 1;
-	// 	if (this.sheet.isReady || this.sheet.isProcessed) this._attachMessage3();
-	// 	const result = this.sheet.process();
-	// 	if (this.sheet.isProcessed) {
-	// 		// on endless we reuse message
-	// 		if (!this.trigger.isEndless) this._msgHandler.next();
-	// 		this._detachMessage2();
-	// 	}
-	// 	return result;
-	// }
-	_attachMessage3() {
+
+	_attachMessage2() {
 		if (this._msgHandler.isProcessed) {
 			let newMessage;
 			const oldMessage = this._msgHandler.message;
@@ -196,35 +138,6 @@ class StreamSheet2 extends StreamSheet {
 		if (message === this._msgHandler.message && !this._msgHandler.isProcessed) this._msgHandler.reset();
 		else this._attachMessage(message);
 	}
-	_attachMessage(message) {
-		if (message) {
-			this.stats.messages += 1;
-			this._msgHandler.message = message;
-			this._emitMessageEvent('message_attached', message);
-		}
-	}
-	_attachMessage2(message) {
-		// get new message if old one is processed and a new one exists
-		if (this._msgHandler.isProcessed) {
-			let newMessage;
-			const oldMessage = this._msgHandler.message;
-			if (this.inbox.size > 1) {
-				if (oldMessage) this.inbox.pop(oldMessage.id);
-				newMessage = this.inbox.peek();
-			}
-			if (message) newMessage = message;
-			if (newMessage) {
-				this.stats.messages += 1; // newMessage ? 1 : 0;
-				this._msgHandler.message = newMessage;
-				this._emitMessageEvent('message_attached', newMessage);
-			}
-		}
-	}
-	// _attachMessage(message) {
-	// 	this.stats.messages += message ? 1 : 0;
-	// 	this._msgHandler.message = message;
-	// 	this._emitMessageEvent('message_attached', message);
-	// }
 	_detachMessage2() {
 		// get mark message as detached if its processed
 		if (this._msgHandler.isProcessed) {
