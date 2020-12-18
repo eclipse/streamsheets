c619e700c9f7e2a92cbf43fab6a814531d2b80e0
tenhouben <arnd.houben@cedalo.com>
2020-12-18 10:51:50 +0100

    machine-core: add new OneTimeTrigger

---
+++ packages/machine-core/src/machine/sheettrigger/OneTimeTrigger.js
@@ -0,0 +1,33 @@
+/********************************************************************************
+ * Copyright (c) 2020 Cedalo AG
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ ********************************************************************************/
+const AbstractTrigger = require('./AbstractTrigger');
+
+const TYPE_CONF = Object.freeze({ type: 'once' });
+
+// FOR DEBUGGING PURPOSE ONLY
+class OneTimeTrigger extends AbstractTrigger {
+	static get TYPE() {
+		return TYPE_CONF.type;
+	}
+	constructor(cfg = {}) {
+		super(Object.assign(cfg, TYPE_CONF));
+		this._isTriggered = false;
+	}
+
+	step() {
+		if (!this._isTriggered) {
+			this._isTriggered = true;
+			this.trigger();
+		}
+	}
+}
+
+module.exports = OneTimeTrigger;
