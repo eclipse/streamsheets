9bacdecfe748ac3ce8721166dbd9005d5025739a
tenhouben <arnd.houben@cedalo.com>
2021-01-27 14:00:29 +0100

    machine-core: clean up utility test mock for execute

--- packages/machine-core/test/_functions/execute.mock.js
+++ packages/machine-core/test/_functions/execute.mock.js
@@ -20,11 +20,6 @@ const execute = (sheet, ...terms) => {
 		callingStreamSheet.resumeProcessing(retval);
 	}
 
-	const doExecute = (calledStreamSheet, pace) => (context) => {
-		const message = context.msgdata ? new Message(context.msgdata) : context.message;
-		calledStreamSheet.execute(context.resumeFn, message, pace, context.repetitions);
-	};
-
 	if (sheet.isProcessing) {
 		const machine = sheet.machine;
 		const context = execute.context;
@@ -32,29 +27,27 @@ const execute = (sheet, ...terms) => {
 		const calledStreamSheet = machine.getStreamSheetByName(terms[0].value);
 		const repetitions = terms[1] ? terms[1].value : 1;
 		const pace = terms[3] != null ? !!terms[3].value : null;
+		let message;
 		if (terms[2]) {
 			const msgdata = terms[2].value;
 			if (typeof msgdata === 'string') {
 				const box = msgdata.startsWith('in:') ? callingStreamSheet.inbox : machine.outbox;
 				const msgId = msgdata.startsWith('in:') ? msgdata.substr(3) : msgdata.substr(4);
-				context.message = box.peek(msgId);
-			} else {
-				context.msgdata = msgdata;
+				message = box.peek(msgId);
+			} else if(msgdata) {
+				message = new Message(msgdata);
 			}
 		}
 		if (!context.initialized) {
 			context.initialized = true;
-			context.repetitions = Math.max(1, repetitions);
 			context.resumeFn = resumeFromExecute(context, callingStreamSheet);
-			context.repeatExecute = doExecute(calledStreamSheet, pace);
 			context.addDisposeListener(cancelExecute(calledStreamSheet));
 		}
 		if (!sheet.isPaused) {
 			// always pause calling sheet, since we do not know how long execute takes (due to pause/sleep functions)
 			callingStreamSheet.pauseProcessing();
-			context.repeatExecute(context);
-		}
-		
+			calledStreamSheet.execute(context.resumeFn, message, pace, repetitions);	
+		}		
 
 		return true;
 	}
