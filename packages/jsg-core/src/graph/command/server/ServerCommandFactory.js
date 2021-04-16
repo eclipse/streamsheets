495e8147cf57fc18f08a01b1b59fa8f117a9e638
tenhouben <arnd.houben@cedalo.com>
2021-04-16 14:38:47 +0200

    jsg-core: map old cell format commands to new ones in ServerCommandFactory

--- packages/jsg-core/src/graph/command/server/ServerCommandFactory.js
+++ packages/jsg-core/src/graph/command/server/ServerCommandFactory.js
@@ -17,12 +17,61 @@ const SetCellsPropertiesCommand = require('./SetCellsPropertiesCommand');
 
 const { getCellRangesFromSelection, toCellsColsRows } = require('../utils');
 
+const getSheet = (ranges) => (ranges[0] ? ranges[0].getSheet() : undefined);
+
+const mapProperitesCommand = (type) => (ranges, map) => {
+	const sheet = getSheet(ranges);
+	const cellsColsRows = toCellsColsRows(ranges, sheet);
+	const properties = { [type]: map };
+	return new SetCellsPropertiesCommand(sheet, cellsColsRows, properties);
+};
+const mapDeleteCellContentCommand = (sheet, selref, action) => {
+	// action: 'all', 'formats', 'values' (, 'formulas'??)
+	if (action === 'values') {
+		return new DeleteCellsCommand(sheet, selref);
+	}
+	const ranges = getCellRangesFromSelection(selref, sheet);
+	const cellsColsRows = toCellsColsRows(ranges, sheet);
+	const propsCmd = new SetCellsPropertiesCommand(sheet, cellsColsRows);
+	if (action === 'all') {
+		return new CompoundRequestCommand(sheet).add(new DeleteCellsCommand(sheet, selref)).add(propsCmd);
+	}
+	return action === 'formats' ? propsCmd : undefined;
+};
+const mapSetCellsCommand = (sheet, cellrefs /* , execute */) => new SetCellsCommand(sheet, cellrefs);
+const mapSetCellLevelsCommand = (sheet, ranges, down) => new SetCellLevelsCommand(sheet, ranges, down);
+const mapSetCellDataCommand = (sheet, cellref, expression /* , execute */) => {
+	const descr = { reference: cellref };
+	if (expression) {
+		descr.value = expression.getValue();
+		descr.formula = expression.hasFormula() ? expression.getFormula() : undefined;
+	}
+	return new SetCellsCommand(sheet, [descr]);
+};
+
+const MapCommands = {
+	'command.CellAttributesCommand': mapProperitesCommand('attributes'),
+	'command.DeleteCellContentCommand': mapDeleteCellContentCommand,
+	'command.DeleteCellsCommand': mapDeleteCellContentCommand,
+	'command.FormatCellsCommand': mapProperitesCommand('formats'),
+	'command.SetCellsCommand': mapSetCellsCommand,
+	'command.SetCellDataCommand': mapSetCellDataCommand,
+	'command.SetCellLevelsCommand': mapSetCellLevelsCommand,
+	'command.TextFormatCellsCommand': mapProperitesCommand('textFormats')
+};
 const Registry = {
 	'command.server.DeleteCellsCommand': DeleteCellsCommand,
 	'command.server.PasteCellsCommand': PasteCellsCommand,
 	'command.server.SetCellsCommand': SetCellsCommand,
 	'command.server.SetCellLevelsCommand': SetCellLevelsCommand,
 	'command.server.SetCellsPropertiesCommand': SetCellsPropertiesCommand
+};
+
+const createCommand = (name, args) => {
+	const Cmd = Registry[name];
+	if (Cmd) return new Cmd(...args);
+	const mapper = MapCommands[name];
+	return mapper ? mapper(...args) : undefined;
 };
 
 const ServerCommandFactory = (basefactory) =>
@@ -33,44 +82,9 @@ const ServerCommandFactory = (basefactory) =>
 				? cmd.createFromObject(data, { graph, viewer, factory: this })
 				: super.createCommand(graph, data, viewer);
 		}
-
-		static createPropertiesCommand(ranges, propsmap, propstype) {
-			propstype = propstype === 'border' ? 'formats' : propstype;
-			const sheet = ranges[0] ? ranges[0].getSheet() : undefined;
-			const cellsColsRows = toCellsColsRows(ranges, sheet);
-			const properties = propstype ? { [propstype]: propsmap } : propsmap;
-			return new SetCellsPropertiesCommand(sheet, cellsColsRows, properties);
-		}
-
-		static createSetCellLevelsCommand(sheet, ranges, down) {
-			return new SetCellLevelsCommand(sheet, ranges, down);
-		}
-
-		static createDeleteCellsCommand(type, sheet, selref, action) {
-			// action: 'all', 'formats', 'values' (, 'formulas'??)
-			if (action === 'values') {
-				return new DeleteCellsCommand(sheet, selref);
-			}
-			const ranges = getCellRangesFromSelection(selref, sheet);
-			const cellsColsRows = toCellsColsRows(ranges, sheet);
-			const propsCmd = new SetCellsPropertiesCommand(sheet, cellsColsRows);
-			if (action === 'all') {
-				return new CompoundRequestCommand(sheet).add(new DeleteCellsCommand(sheet, selref)).add(propsCmd);
-			}
-			return action === 'formats' ? propsCmd : undefined;
-		}
-
-		static createSetCellsCommand(sheet, cellrefs /* , execute */) {
-			return new SetCellsCommand(sheet, cellrefs);
-		}
-		static createSetCellDataCommand(sheet, cellref, expression /* , execute */) {
-			const descr = { reference: cellref };
-			if (expression) {
-				descr.value = expression.getValue();
-				descr.formula = expression.hasFormula() ? expression.getFormula() : undefined;
-			}
-			return new SetCellsCommand(sheet, [descr]);
+		static create(cmdname, ...args) {
+			return createCommand(cmdname, args) || super.create(cmdname, ...args);
 		}
 	};
 
-	module.exports = ServerCommandFactory;
+module.exports = ServerCommandFactory;
