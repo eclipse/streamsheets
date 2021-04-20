81c55687f402a7e79ad02714bdf0447753f26e42
cabane-mr <michael.raue@cedalo.com>
2021-04-20 11:53:26 +0200

    add tooltip for stream actions

--- packages/webui/src/components/Dashboard/StreamTableRow.js
+++ packages/webui/src/components/Dashboard/StreamTableRow.js
@@ -21,17 +21,12 @@ import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import AddCircle from '@material-ui/icons/AddCircleOutline';
 import StreamHelper from '../../helper/StreamHelper';
 import { FormattedMessage } from 'react-intl';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const styles = () => ({
 	typoRoot: {
-		fontSize: '9pt',
-		fontWeight: 'bold',
-		margin: '0px',
-		minWidth: '75px',
-		paddingBottom: '7px',
-		paddingTop: '7px'
-	},
-	sectionRoot: {
+		fontSize: '9pt', fontWeight: 'bold', margin: '0px', minWidth: '75px', paddingBottom: '7px', paddingTop: '7px'
+	}, sectionRoot: {
 		display: 'flex',
 		margin: '10px 0px 0px 40px',
 		borderBottom: `1px solid #e0e0e0`,
@@ -50,266 +45,343 @@ const StreamTableRow = (props) => {
 	if (row.open) {
 		setOpen(true);
 		row.open = undefined;
-		return (<div/>);
+		return (<div />);
 	}
 	const openConnector = open;
 
-	return (
-		<React.Fragment>
-			<TableRow
-				style={{
-					textDecoration: row.disabled ? 'line-through' : 'inherit',
-					height: '40px'
-				}}
-				hover
-				classes={{ root: classes.tableRoot }}
-				key={row.id}
-			>
-				<TableCell style={{ width: '20px' }} padding="none" align="left">
-					<IconButton
-						style={{ margin: '0px 5px', padding: '4px' }}
-						aria-label="expand row"
-						size="small"
-						onClick={() => setOpen(!open)}
-					>
-						{openConnector ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-					</IconButton>
-				</TableCell>
-				<TableCell
-					onClick={() => setOpen(!openConnector)}
-					style={{ cursor: 'pointer', fontWeight: 'bold' }}
-					padding="none"
-					component="th"
-					scope="row"
-					align="left"
+	return (<React.Fragment>
+		<TableRow
+			style={{
+				textDecoration: row.disabled ? 'line-through' : 'inherit', height: '40px'
+			}}
+			hover
+			classes={{ root: classes.tableRoot }}
+			key={row.id}
+		>
+			<TableCell style={{ width: '20px' }} padding='none' align='left'>
+				<IconButton
+					style={{ margin: '0px 5px', padding: '4px' }}
+					aria-label='expand row'
+					size='small'
+					onClick={() => setOpen(!open)}
 				>
-					{row.name}
-				</TableCell>
-				<TableCell style={{ cursor: 'pointer'}} onClick={() => setOpen(!openConnector)} padding="none" align="left">
-					{row.provider.name}
-				</TableCell>
-				<TableCell style={{ cursor: 'pointer'}} onClick={() => setOpen(!openConnector)} padding="none" align="left">
-					{row.url}
-				</TableCell>
-				<TableCell style={{ cursor: 'pointer', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '1000px'}} onClick={() => setOpen(!openConnector)} padding="none" align="left">
-					{row.topic}
-				</TableCell>
-				<TableCell style={{ cursor: 'pointer'}} onClick={() => setOpen(!openConnector)} padding="none" align="left">
-					{row.lastModified}
-				</TableCell>
-				<TableCell padding="none" align="left">
+					{openConnector ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+				</IconButton>
+			</TableCell>
+			<TableCell
+				onClick={() => setOpen(!openConnector)}
+				style={{ cursor: 'pointer', fontWeight: 'bold' }}
+				padding='none'
+				component='th'
+				scope='row'
+				align='left'
+			>
+				{row.name}
+			</TableCell>
+			<TableCell style={{ cursor: 'pointer' }} onClick={() => setOpen(!openConnector)} padding='none'
+					   align='left'>
+				{row.provider.name}
+			</TableCell>
+			<TableCell style={{ cursor: 'pointer' }} onClick={() => setOpen(!openConnector)} padding='none'
+					   align='left'>
+				{row.url}
+			</TableCell>
+			<TableCell
+				style={{ cursor: 'pointer', overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '1000px' }}
+				onClick={() => setOpen(!openConnector)} padding='none' align='left'>
+				{row.topic}
+			</TableCell>
+			<TableCell style={{ cursor: 'pointer' }} onClick={() => setOpen(!openConnector)} padding='none'
+					   align='left'>
+				{row.lastModified}
+			</TableCell>
+			<TableCell padding='none' align='left'>
+				<Tooltip
+					enterDelay={300}
+					title={<FormattedMessage id='Dashboard.EditConnector' defaultMessage='Edit Connector' />}
+				>
 					<IconButton
 						style={{ padding: '4px' }}
-						size="small"
+						size='small'
 						onClick={() => props.onStreamOpen(row, 'connector')}
 					>
 						<IconEdit />
 					</IconButton>
+				</Tooltip>
+				<Tooltip
+					enterDelay={300}
+					title={<FormattedMessage id='Dashboard.DeleteConnector' defaultMessage='Delete Connector' />}
+				>
 					<IconButton
 						style={{ padding: '4px' }}
-						size="small"
+						size='small'
 						onClick={() => props.onStreamDelete(row, 'connectors')}
 					>
 						<IconDelete />
 					</IconButton>
-					<IconButton style={{ padding: '4px' }} size="small" onClick={() => props.onStreamReload(row)}>
+				</Tooltip>
+				<Tooltip
+					enterDelay={300}
+					title={<FormattedMessage id='Dashboard.ReloadConnector' defaultMessage='Reload Connector' />}
+				>
+					<IconButton style={{ padding: '4px' }} size='small' onClick={() => props.onStreamReload(row)}>
 						<IconReload />
 					</IconButton>
-				</TableCell>
-			</TableRow>
-			<TableRow key={`sub${row.id}`} style={{ height: '0px' }}>
-				<TableCell
-					style={{ paddingBottom: openConnector ? '6px' : '0px', paddingTop: '0px', paddingLeft: '0px', paddingRight: '0px' }}
-					colSpan={7}
-				>
-					<Collapse in={openConnector} timeout="auto" unmountOnExit>
-						<Paper square elevation={1} style={{maxHeight: 'none', maxWidth: 'none'}}>
-							{ row.provider.canConsume ? (
-								<React.Fragment>
+				</Tooltip>
+			</TableCell>
+		</TableRow>
+		<TableRow key={`sub${row.id}`} style={{ height: '0px' }}>
+			<TableCell
+				style={{
+					paddingBottom: openConnector ? '6px' : '0px',
+					paddingTop: '0px',
+					paddingLeft: '0px',
+					paddingRight: '0px'
+				}}
+				colSpan={7}
+			>
+				<Collapse in={openConnector} timeout='auto' unmountOnExit>
+					<Paper square elevation={1} style={{ maxHeight: 'none', maxWidth: 'none' }}>
+						{row.provider.canConsume ? (<React.Fragment>
 							<div className={classes.sectionRoot}>
 								<Typography
 									classes={{ root: classes.typoRoot }}
-									color="textSecondary"
-									variant="body2"
+									color='textSecondary'
+									variant='body2'
 									gutterBottom
-									component="div"
+									component='div'
 								>
-									<FormattedMessage id="Dashboard.consumers" defaultMessage="Consumers" />
+									<FormattedMessage id='Dashboard.consumers' defaultMessage='Consumers' />
 								</Typography>
-								<IconButton
-									style={{ padding: '2px' }}
-									size="small"
-									onClick={() => handleClick('consumer')}
+								<Tooltip
+									enterDelay={300}
+									title={<FormattedMessage id='Dashboard.NewConsumer'
+															 defaultMessage='New Consumer' />}
 								>
-									<AddCircle />
-								</IconButton>
+									<IconButton
+										style={{ padding: '2px' }}
+										size='small'
+										onClick={() => handleClick('consumer')}
+									>
+										<AddCircle />
+									</IconButton>
+								</Tooltip>
 							</div>
-							<Table size="small" aria-label="purchases">
+							<Table size='small' aria-label='purchases'>
 								<TableBody>
-									{row.consumers.map((historyRow) => (
-										<TableRow
-											style={{
-												textDecoration: row.disabled || historyRow.disabled ? 'line-through' : 'inherit',
-											}}
-											hover
-											key={historyRow.id}
+									{row.consumers.map((historyRow) => (<TableRow
+										style={{
+											textDecoration: row.disabled || historyRow.disabled ? 'line-through' :
+												'inherit'
+										}}
+										hover
+										key={historyRow.id}
+									>
+										<TableCell
+											style={{ width: '40px', borderBottom: 'none' }}
+											padding='none'
+											align='left'
+										/>
+										<TableCell
+											style={{ cursor: 'pointer', width: '12%', minWidth: '250px' }}
+											padding='none'
+											align='left'
+											onClick={() => props.onStreamOpen(historyRow, 'consumer')}
 										>
-											<TableCell
-												style={{ width: '40px', borderBottom: 'none' }}
-												padding="none"
-												align="left"
+											<img
+												style={{ verticalAlign: 'bottom', paddingRight: '6px' }}
+												width={15}
+												height={15}
+												src={StreamHelper.getIconForState(historyRow.state)}
+												alt='state'
 											/>
-											<TableCell
-												style={{ cursor: 'pointer', width: '12%', minWidth: '250px' }}
-												padding="none"
-												align="left"
-												onClick={() => props.onStreamOpen(historyRow, 'consumer')}
+											{historyRow.name}
+										</TableCell>
+										<TableCell style={{ width: '10%', minWidth: '170px' }} padding='none'
+												   align='left'>
+											{historyRow.provider.name}
+										</TableCell>
+										<TableCell style={{ width: '20%' }} padding='none' align='left'>
+											{historyRow.url}
+										</TableCell>
+										<TableCell padding='none' align='left' style={{
+											overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '1000px'
+										}}>
+											{historyRow.topic}
+										</TableCell>
+										<TableCell style={{ width: '120px', minWidth: '120px' }} padding='none'
+												   align='left'>
+											{historyRow.lastModified}
+										</TableCell>
+										<TableCell style={{ width: '95px', minWidth: '95px' }} padding='none'
+												   align='left'>
+											<Tooltip
+												enterDelay={300}
+												title={<FormattedMessage id='Dashboard.EditConsumer'
+																		 defaultMessage='Edit Consumer' />}
 											>
-												<img
-													style={{ verticalAlign: 'bottom', paddingRight: '6px' }}
-													width={15}
-													height={15}
-													src={StreamHelper.getIconForState(historyRow.state)}
-													alt="state"
-												/>
-												{historyRow.name}
-											</TableCell>
-											<TableCell style={{ width: '10%', minWidth: '170px' }} padding="none" align="left">
-												{historyRow.provider.name}
-											</TableCell>
-											<TableCell style={{ width: '20%' }} padding="none" align="left">
-												{historyRow.url}
-											</TableCell>
-											<TableCell padding="none" align="left" style={{overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '1000px'}}>
-												{historyRow.topic}
-											</TableCell>
-											<TableCell style={{ width: '120px', minWidth: '120px' }} padding="none" align="left">
-												{historyRow.lastModified}
-											</TableCell>
-											<TableCell style={{ width: '95px', minWidth: '95px' }} padding="none" align="left">
 												<IconButton
 													style={{ padding: '4px' }}
-													size="small"
+													size='small'
 													onClick={() => props.onStreamOpen(historyRow, 'consumer')}
 												>
 													<IconEdit />
 												</IconButton>
+											</Tooltip>
+											<Tooltip
+												enterDelay={300}
+												title={<FormattedMessage id='Dashboard.DeleteConsumer'
+																		 defaultMessage='Delete Consumer' />}
+											>
 												<IconButton
 													style={{ padding: '4px' }}
-													size="small"
+													size='small'
 													onClick={() => props.onStreamDelete(historyRow, 'consumers')}
 												>
 													<IconDelete />
 												</IconButton>
+											</Tooltip>
+											<Tooltip
+												enterDelay={300}
+												title={<FormattedMessage id='Dashboard.ReloadConsumer'
+																		 defaultMessage='Reload Consumer' />}
+											>
 												<IconButton
 													style={{ padding: '4px' }}
-													size="small"
+													size='small'
 													onClick={() => props.onStreamReload(historyRow)}
 												>
 													<IconReload />
 												</IconButton>
-											</TableCell>
-										</TableRow>
-									))}
+											</Tooltip>
+										</TableCell>
+									</TableRow>))}
 								</TableBody>
 							</Table>
-								</React.Fragment>) : null}
-							{ row.provider.canProduce ? (
-								<React.Fragment>
+						</React.Fragment>) : null}
+						{row.provider.canProduce ? (<React.Fragment>
 							<div className={classes.sectionRoot}>
 								<Typography
 									classes={{ root: classes.typoRoot }}
-									color="textSecondary"
-									variant="body2"
+									color='textSecondary'
+									variant='body2'
 									gutterBottom
-									component="div"
+									component='div'
 								>
-									<FormattedMessage id="Dashboard.producers" defaultMessage="Producers" />
+									<FormattedMessage id='Dashboard.producers' defaultMessage='Producers' />
 								</Typography>
-								<IconButton
-									style={{ padding: '2px' }}
-									size="small"
-									onClick={() => handleClick('producer')}
+								<Tooltip
+									enterDelay={300}
+									title={<FormattedMessage id='Dashboard.NewProducer'
+															 defaultMessage='Edit Producer' />}
 								>
-									<AddCircle />
-								</IconButton>
+									<IconButton
+										style={{ padding: '2px' }}
+										size='small'
+										onClick={() => handleClick('producer')}
+									>
+										<AddCircle />
+									</IconButton>
+								</Tooltip>
 							</div>
-							<Table size="small" aria-label="purchases">
+							<Table size='small' aria-label='purchases'>
 								<TableBody>
-									{row.producers.map((historyRow) => (
-										<TableRow
-											style={{
-												textDecoration: row.disabled || historyRow.disabled ? 'line-through' : 'inherit',
-											}}
-											hover
-											key={historyRow.id}
+									{row.producers.map((historyRow) => (<TableRow
+										style={{
+											textDecoration: row.disabled || historyRow.disabled ? 'line-through' :
+												'inherit'
+										}}
+										hover
+										key={historyRow.id}
+									>
+										<TableCell
+											style={{ width: '40px', borderBottom: 'none' }}
+											padding='none'
+											align='left'
+										/>
+										<TableCell
+											style={{ cursor: 'pointer', width: '12%', minWidth: '250px' }}
+											padding='none'
+											align='left'
+											onClick={() => props.onStreamOpen(historyRow, 'producer')}
 										>
-											<TableCell
-												style={{ width: '40px', borderBottom: 'none' }}
-												padding="none"
-												align="left"
+											<img
+												style={{ verticalAlign: 'bottom', paddingRight: '6px' }}
+												width={15}
+												height={15}
+												src={StreamHelper.getIconForState(historyRow.state)}
+												alt='state'
 											/>
-											<TableCell
-												style={{ cursor: 'pointer', width: '12%', minWidth: '250px'  }}
-												padding="none"
-												align="left"
-												onClick={() => props.onStreamOpen(historyRow, 'producer')}
+											{historyRow.name}
+										</TableCell>
+										<TableCell style={{ width: '10%', minWidth: '170px' }} padding='none'
+												   align='left'>
+											{historyRow.provider.name}
+										</TableCell>
+										<TableCell style={{ width: '20%' }} padding='none' align='left'>
+											{historyRow.url}
+										</TableCell>
+										<TableCell padding='none' align='left' style={{
+											overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '1000px'
+										}}>
+											{historyRow.topic}
+										</TableCell>
+										<TableCell style={{ width: '120px', minWidth: '120px' }} padding='none'
+												   align='left'>
+											{historyRow.lastModified}
+										</TableCell>
+										<TableCell style={{ width: '95px', minWidth: '95px' }} padding='none'
+												   align='left'>
+											<Tooltip
+												enterDelay={300}
+												title={<FormattedMessage id='Dashboard.EditProducer'
+																		 defaultMessage='Edit Producer' />}
 											>
-												<img
-													style={{ verticalAlign: 'bottom', paddingRight: '6px' }}
-													width={15}
-													height={15}
-													src={StreamHelper.getIconForState(historyRow.state)}
-													alt="state"
-												/>
-												{historyRow.name}
-											</TableCell>
-											<TableCell style={{ width: '10%', minWidth: '170px' }} padding="none" align="left">
-												{historyRow.provider.name}
-											</TableCell>
-											<TableCell style={{ width: '20%' }} padding="none" align="left">
-												{historyRow.url}
-											</TableCell>
-											<TableCell padding="none" align="left" style={{overflow: 'hidden', textOverflow: 'ellipsis', maxWidth: '1000px'}}>
-												{historyRow.topic}
-											</TableCell>
-											<TableCell style={{ width: '120px', minWidth: '120px' }} padding="none" align="left">
-												{historyRow.lastModified}
-											</TableCell>
-											<TableCell style={{ width: '95px', minWidth: '95px' }} padding="none" align="left">
 												<IconButton
 													style={{ padding: '4px' }}
-													size="small"
+													size='small'
 													onClick={() => props.onStreamOpen(historyRow, 'producer')}
 												>
 													<IconEdit />
+
 												</IconButton>
+											</Tooltip>
+											<Tooltip
+												enterDelay={300}
+												title={<FormattedMessage id='Dashboard.DeleteProducer'
+																		 defaultMessage='Delete Producer' />}
+											>
 												<IconButton
 													style={{ padding: '4px' }}
-													size="small"
+													size='small'
 													onClick={() => props.onStreamDelete(historyRow, 'producers')}
 												>
 													<IconDelete />
 												</IconButton>
+											</Tooltip>
+											<Tooltip
+												enterDelay={300}
+												title={<FormattedMessage id='Dashboard.ReloadProducer'
+																		 defaultMessage='Reload Producer' />}
+											>
 												<IconButton
 													style={{ padding: '4px' }}
-													size="small"
+													size='small'
 													onClick={() => props.onStreamReload(historyRow)}
 												>
 													<IconReload />
 												</IconButton>
-											</TableCell>
-										</TableRow>
-									))}
+											</Tooltip>
+										</TableCell>
+									</TableRow>))}
 								</TableBody>
 							</Table>
-								</React.Fragment>) : null}
-						</Paper>
-					</Collapse>
-				</TableCell>
-			</TableRow>
-		</React.Fragment>
-	);
+						</React.Fragment>) : null}
+					</Paper>
+				</Collapse>
+			</TableCell>
+		</TableRow>
+	</React.Fragment>);
 };
 
 StreamTableRow.propTypes = {
