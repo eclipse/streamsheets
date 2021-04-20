c2b1c4ba14c2c10b827c16e890b09b164842cabc
tenhouben <arnd.houben@cedalo.com>
2021-04-20 13:19:48 +0200

    machine-core: support optional category property in functions help (DL-4897)

--- packages/machine-core/src/FunctionRegistry.js
+++ packages/machine-core/src/FunctionRegistry.js
@@ -1,7 +1,7 @@
 /********************************************************************************
  * Copyright (c) 2020 Cedalo AG
  *
- * This program and the accompanying materials are made available under the 
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
@@ -24,6 +24,8 @@ const requireModule = async (path) => {
 
 let functionFactory;
 
+const DEF_CATEGORY = ['other', { en: 'Other', de: 'Sonstige' }];
+
 const Functions = {
 	core: {},
 	additional: {},
@@ -34,6 +36,25 @@ const Actions = {
 	additional: {}
 };
 
+const addFunction = (category, [key, value]) => {
+	category.functions[key] = value;
+	return category;
+};
+const addFunctions = (functions, category) => Object.entries(functions).reduce(addFunction, category);
+
+const addCategory = (category, categories) => {
+	const [name, locales] = Object.entries(category)[0] || DEF_CATEGORY;
+	if (categories[name] == null) categories[name] = {};
+	if (categories[name].functions == null) categories[name].functions = {};
+	return Object.assign(categories[name], locales);
+};
+const addHelp = (help = {}, categories = {}) => {
+	const { category = {}, functions = {} } = help;
+	const newCategory = addCategory(category, categories);
+	addFunctions(functions, newCategory);
+	return categories;
+};
+
 const registerCore = ({ actions = {}, functions = {}, FunctionFactory } = {}) => {
 	Actions.core = Object.assign(Actions.core, actions);
 	Functions.core = Object.assign(Functions.core, functions);
@@ -42,7 +63,8 @@ const registerCore = ({ actions = {}, functions = {}, FunctionFactory } = {}) =>
 const registerAdditional = ({ actions = {}, functions = {}, help = {} } = {}) => {
 	Actions.additional = Object.assign(Actions.additional, actions);
 	Functions.additional = Object.assign(Functions.additional, functions);
-	Functions.additionalHelp = Object.assign(Functions.additionalHelp, help);
+	if (Array.isArray(help)) help.forEach((fnHelp) => addHelp(fnHelp, Functions.additionalHelp));
+	else addHelp(help, Functions.additionalHelp);
 };
 const logError = (err, mod) => logger.error(`Failed to load module: '${moduleName(mod)}'! Reason: ${err.message}`);
 
