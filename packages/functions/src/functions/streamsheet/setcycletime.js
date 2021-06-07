4514e71474cd43055b0503d16ef763a8bd10ad8c
tenhouben <arnd.houben@cedalo.com>
2021-06-07 12:41:40 +0200

    functions: setcycletime() should only be executed on sheet calculation (DL-5036)

--- packages/functions/src/functions/streamsheet/setcycletime.js
+++ packages/functions/src/functions/streamsheet/setcycletime.js
@@ -1,21 +1,22 @@
 /********************************************************************************
  * Copyright (c) 2020 Cedalo AG
  *
- * This program and the accompanying materials are made available under the 
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
  * SPDX-License-Identifier: EPL-2.0
  *
  ********************************************************************************/
-const { runFunction, sheet: { getMachine } } = require('../../utils');
 const { convert } = require('@cedalo/commons');
 const { FunctionErrors } = require('@cedalo/error-codes');
+const { runFunction, sheet: { getMachine } } = require('../../utils');
 
 const ERROR = FunctionErrors.code;
 
 const setcycletime = (sheet, ...terms) =>
 	runFunction(sheet, terms)
+		.onSheetCalculation()
 		.withArgCount(1)
 		.mapNextArg((cycletime) => {
 			const time = convert.toNumber(cycletime.value);
@@ -26,6 +27,5 @@ const setcycletime = (sheet, ...terms) =>
 			machine.cycletime = cycletime;
 			return true;
 		});
-
 
 module.exports = setcycletime;
