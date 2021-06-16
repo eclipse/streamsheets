3d57b530596efe3f51d521d8bc6a42503c2fb3c7
Philip Ackermann <info@philipackermann.de>
2021-06-16 18:16:15 +0200

    Add more metrics

--- packages/metrics/src/MetricsManager.js
+++ packages/metrics/src/MetricsManager.js
@@ -8,27 +8,43 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  ********************************************************************************/
- const os = require('os');
+
+const os = require('os');
+const v8 = require('v8');
+const system = require('systeminformation');
 
 module.exports = class MetricsManager {
-	static getMetrics() {
+	static async getMetrics() {
+		const currentLoad = await system.currentLoad();
 		const { pid, title, versions } = process;
 		const memory = process.memoryUsage();
 		const cpu = process.cpuUsage();
+		const cpus = os.cpus();
 		const resource = process.resourceUsage();
 		const report = process.report.getReport();
 
+		const heapStatistics = v8.getHeapStatistics();
+		const heapCodeStatistics = v8.getHeapCodeStatistics();
+		const heapSpaceStatistics = v8.getHeapSpaceStatistics();
+
 		const result = {
+			currentLoad,
 			title,
 			pid,
 			versions,
 			memory,
 			cpu,
+			cpus,
 			resource,
 			report,
 			os: {
 				freemem: os.freemem(),
 				totalmem: os.totalmem()
+			},
+			v8: {
+				heapStatistics,
+				heapCodeStatistics,
+				heapSpaceStatistics
 			}
 		}
 		return result;
