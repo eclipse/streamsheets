eb631bb387353559e934017673d8d388d0a2a83b
tenhouben <arnd.houben@cedalo.com>
2021-06-16 16:56:56 +0200

    commons: typo

--- packages/commons/src/proc.js
+++ packages/commons/src/proc.js
@@ -2,12 +2,12 @@
 // const path = require('path');
 // const getScript = () => path.relative(process.cwd(), process.argv[1]);
 
-const getScriptArgs = () => process.argv.slice(2).join(' ');
-const getProcessArgs = () => process.execArgv.join(' ');
+const getScriptArgs = () => process.argv ? ` ${process.argv.slice(2).join(' ')}` : '';
+const getProcessArgs = () => process.execArgv ? ` ${process.execArgv.join(' ')}` : '';
 
 const setProcessTitle = (title, includeArgs = true) => {
-	title = title || process.args[0];
-	process.title = includeArgs ? `Cedalo/${title} ${getProcessArgs()} ${getScriptArgs()}` : `${title}`;
+	title = title || process.argv[0];
+	process.title = includeArgs ? `Cedalo/${title}${getProcessArgs()}${getScriptArgs()}` : `${title}`;
 };
 
 module.exports = {
