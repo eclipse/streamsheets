62748d6d76aa0c0f179b41789e552ad6c1115f7d
tenhouben <arnd.houben@cedalo.com>
2021-06-21 12:17:22 +0200

    machine-core: tweak log output for autoexit

--- packages/machine-core/src/utils/autoexit.js
+++ packages/machine-core/src/utils/autoexit.js
@@ -6,16 +6,16 @@ const AUTO_EXIT_TIMEOUT = process.env.AUTO_EXIT_TIMEOUT || 60 * 1000;
 let timeoutId;
 const cancel = (mname) => {
 	if (timeoutId) {
-		logger.info(`clear exit timer for machine: ${mname}`);
+		logger.info(`clear exit timer for machine${mname}`);
 		clearTimeout(timeoutId);
 		timeoutId = undefined;
 	}
 };
 const update = (machine) => {
-	const mname = `${machine.name} [${machine.id}]`;
+	const mname = `[${machine.id}]: ${machine.name}`;
 	cancel(mname);
 	if (machine.state === State.STOPPED && machine.getClientCount() === 0) {
-		logger.info(`start exit timer for machine: ${mname}`);
+		logger.info(`start exit timer for machine${mname}`);
 		timeoutId = setTimeout(() => {
 			logger.info(`exit machine: ${mname}`);
 			process.exit(1);
