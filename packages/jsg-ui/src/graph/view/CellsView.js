d1d3efb919763b80b7f799330aa5398f9187b1d9
tenhouben <arnd.houben@cedalo.com>
2021-06-21 18:14:42 +0200

    jsg-ui: change color of error info marker if cell is filled red

--- packages/jsg-ui/src/graph/view/CellsView.js
+++ packages/jsg-ui/src/graph/view/CellsView.js
@@ -903,7 +903,7 @@ export default class CellsView extends NodeView {
 		}
 		// draw error marker
 		if (data.errorInfo) {
-			const markerColor = data.hasErrorValue ? '#F4BB38': '#FF0000';
+			const markerColor = data.displayFunctionName && data.hasErrorValue ? '#F4BB38': '#FF0000';
 			drawMarker(graphics, markerColor, [
 				{ x: columnInfo.x, y: rowInfo.y },
 				{ x: columnInfo.x + 300, y: rowInfo.y },
