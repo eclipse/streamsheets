efbd05e5bc15fe428499b69717c1a303d538c608
tenhouben <arnd.houben@cedalo.com>
2021-08-25 19:21:03 +0200

    functions: mstoserial only supports positive numbers (DL-1351)

--- packages/functions/src/functions/date/mstoserial.js
+++ packages/functions/src/functions/date/mstoserial.js
@@ -1,27 +1,25 @@
 /********************************************************************************
  * Copyright (c) 2020 Cedalo AG
  *
- * This program and the accompanying materials are made available under the 
+ * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
  * SPDX-License-Identifier: EPL-2.0
  *
  ********************************************************************************/
-const { convert, serialnumber: { ms2serial } } = require('@cedalo/commons');
+const {	convert, serialnumber: { ms2serial } } = require('@cedalo/commons');
 const { FunctionErrors } = require('@cedalo/error-codes');
-const {	runFunction } = require('../../utils');
+const { runFunction } = require('../../utils');
 
 const ERROR = FunctionErrors.code;
-
 
 const mstoserial = (sheet, ...terms) =>
 	runFunction(sheet, terms)
 		.withMinArgs(1)
 		.withMaxArgs(2)
-		.mapNextArg((ms) => convert.toNumber(ms.value, ERROR.VALUE))
+		.mapNextArg((ms) => convert.toNumberPositive(ms.value, ERROR.VALUE))
 		.mapNextArg((utc) => convert.toBoolean(utc && utc.value, false))
 		.run(ms2serial);
-
 
 module.exports = mstoserial;
