a5da5c8313d30412e9b2d786efa54a8c4a8e241a
Andreas Sayn <andreas.sayn@cedalo.com>
2021-11-09 10:05:00 +0100

    machines: add health check

--- packages/machine-core/src/ipc/RequestHandlerRegistry.js
+++ packages/machine-core/src/ipc/RequestHandlerRegistry.js
@@ -1052,6 +1052,15 @@ class UpdateStreamSheet extends ARequestHandler {
 	}
 }
 
+class HealthCheck extends ARequestHandler {
+	get isModifying() {
+		return false;
+	}
+	handle(msg) {
+		return Promise.resolve();
+	}
+}
+
 class RequestHandlerRegistry {
 	static of(monitor) {
 		const machine = monitor.machine;
@@ -1099,6 +1108,7 @@ class RequestHandlerRegistry {
 		registry.handlers.set('updateMachine', new UpdateMachine(machine, monitor));
 		registry.handlers.set('updateMachineMonitor', new UpdateMachineMonitor(machine, monitor));
 		registry.handlers.set('updateStreamSheet', new UpdateStreamSheet(machine, monitor));
+		registry.handlers.set('healthcheck', new HealthCheck(machine, monitor));
 		return registry;
 	}
 
